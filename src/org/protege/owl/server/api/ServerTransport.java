ad586055cb6028420639dc0a4347ef0a4060a68c
Timothy Redmond <tredmond@stanford.edu>
2010-02-14 20:10:31 +0000

    added comment

--- src/org/protege/owl/server/api/ServerTransport.java
+++ src/org/protege/owl/server/api/ServerTransport.java
@@ -2,6 +2,14 @@ package org.protege.owl.server.api;
 
 import java.io.IOException;
 
+/**
+ * Implementations of this class are responsible for making the server capabilities available on the 
+ * web or internet.
+ * 
+ * @author tredmond
+ *
+ */
+
 public interface ServerTransport {
 
     void initialize(Server server) throws IOException;
