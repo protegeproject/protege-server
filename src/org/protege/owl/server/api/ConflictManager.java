0a812d7fcdadf4043b12f7193a57309c0f85c4ac
Timothy Redmond <tredmond@stanford.edu>
2010-03-15 14:04:10 +0000

    conflict manager needs to know revision infomration

--- src/org/protege/owl/server/api/ConflictManager.java
+++ src/org/protege/owl/server/api/ConflictManager.java
@@ -1,10 +1,11 @@
 package org.protege.owl.server.api;
 
 import java.util.List;
+import java.util.Set;
 
 import org.protege.owl.server.exception.RemoteOntologyChangeException;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
 public interface ConflictManager {
-    void validateChanges(List<OWLOntologyChange> changes) throws RemoteOntologyChangeException;
+    void validateChanges(Set<RemoteOntology> versions, List<OWLOntologyChange> changes) throws RemoteOntologyChangeException;
 }
