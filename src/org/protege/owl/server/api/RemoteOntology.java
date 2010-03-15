33e1c3f2646f76974411e9406cff19ed30867ad9
Timothy Redmond <tredmond@stanford.edu>
2010-03-15 14:01:58 +0000

    removing version infomration which made this class have two dual purposes

--- src/org/protege/owl/server/api/RemoteOntology.java
+++ src/org/protege/owl/server/api/RemoteOntology.java
@@ -8,12 +8,6 @@ public class RemoteOntology implements Serializable {
     private static final long serialVersionUID = 6539431654428651110L;
     private Integer revision;
     private IRI ontologyName;
-    private IRI ontologyVersion;
-    
-    public RemoteOntology(IRI ontologyName, IRI ontologyVersion) {
-        this.ontologyName = ontologyName;
-        this.ontologyVersion = ontologyVersion;
-    }
     
     public RemoteOntology(IRI ontologyName, Integer revision) {
         this.ontologyName = ontologyName;
@@ -28,12 +22,5 @@ public class RemoteOntology implements Serializable {
     }
     public IRI getOntologyName() {
         return ontologyName;
-    }
-    public IRI getOntologyVersion() {
-        return ontologyVersion;
-    }
-    
-    
-    
-    
+    } 
 }
