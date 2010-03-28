72195ced51555bd7a2519819f10b1b6c16bf8db3
Timothy Redmond <tredmond@stanford.edu>
2010-03-28 06:27:32 +0000

    refactor

--- src/org/protege/owl/server/osgi/OSGiConnection.java
+++
@@ -1,8 +0,0 @@
-package org.protege.owl.server.osgi;
-
-import org.osgi.framework.BundleContext;
-import org.protege.owl.server.api.ServerConnection;
-
-public interface OSGiConnection extends ServerConnection {
-    void setBundleContext(BundleContext context);
-}
