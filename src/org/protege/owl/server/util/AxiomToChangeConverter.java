1f2ae304a958444a6afc7228845c80c1ca97bdec
Timothy Redmond <tredmond@stanford.edu>
2010-03-29 18:28:28 +0000

    transfering

--- src/org/protege/owl/server/util/AxiomToChangeConverter.java
+++ src/org/protege/owl/server/util/AxiomToChangeConverter.java
@@ -3,8 +3,14 @@ package org.protege.owl.server.util;
 import java.util.Set;
 
 import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 public class AxiomToChangeConverter extends AxiomAnnotationsVisitor {
+	private OWLOntology ontology;
+	
+	public AxiomToChangeConverter(OWLOntology ontology) {
+		this.ontology = ontology;
+	}
 
 	@Override
 	protected Set<OWLAnnotation> processAnnotations(
