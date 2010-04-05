4a9fe710111969eff4a483cd119503526032e36b
Timothy Redmond <tredmond@stanford.edu>
2010-04-05 19:13:19 +0000

    initial configuration package - will eventually use the Protege 3 metaproject

---
+++ src/org/protege/owl/server/configuration/ServerConfiguration.java
@@ -0,0 +1,8 @@
+package org.protege.owl.server.configuration;
+
+import java.util.HashMap;
+
+public class ServerConfiguration extends HashMap<String, String> {
+	private static final long serialVersionUID = -2667140662344108331L;
+
+}
