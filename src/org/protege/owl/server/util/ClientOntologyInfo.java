cf647ea671795c86f902f0150032b87cefc97a1b
Timothy Redmond <tredmond@stanford.edu>
2010-04-17 05:45:37 +0000

    refactored into a class of its own and added the short name information

---
+++ src/org/protege/owl/server/util/ClientOntologyInfo.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.protege.owl.server.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.semanticweb.owlapi.model.OWLOntologyChange;
+
+public class ClientOntologyInfo {
+	private List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+	private String shortName;
+	private int revision;
+
+	
+	public ClientOntologyInfo(String shortName, int revision) {
+		super();
+		this.shortName = shortName;
+		this.revision = revision;
+	}
+	
+	public String getShortName() {
+        return shortName;
+    }
+	
+	public int getRevision() {
+		return revision;
+	}
+	public void setRevision(int revision) {
+		this.revision = revision;
+	}
+	public List<OWLOntologyChange> getChanges() {
+		return changes;
+	}
+	
+	public void addChange(OWLOntologyChange change) {
+		changes.add(change);
+	}
+	
+	public List<OWLOntologyChange> clearChanges() {
+		List<OWLOntologyChange> oldChanges = changes;
+		changes = new ArrayList<OWLOntologyChange>();
+		return oldChanges;
+	}
+	
+}
