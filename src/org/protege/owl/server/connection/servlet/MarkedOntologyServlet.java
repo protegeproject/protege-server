91239165d6584fe44536fc59cc108e6f01066cda
Timothy Redmond <tredmond@stanford.edu>
2010-04-17 05:43:06 +0000

    refactor of exceptions

--- src/org/protege/owl/server/connection/servlet/MarkedOntologyServlet.java
+++ src/org/protege/owl/server/connection/servlet/MarkedOntologyServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.protege.owl.server.api.RemoteOntologyRevisions;
 import org.protege.owl.server.api.Server;
-import org.protege.owl.server.exception.RemoteOntologyCreationException;
 
 public class MarkedOntologyServlet extends HttpServlet {
     private static final long serialVersionUID = -6826342078353081611L;
@@ -29,30 +28,23 @@ public class MarkedOntologyServlet extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            String encoded = request.getPathInfo();
-            StringTokenizer tokenizer = new StringTokenizer(encoded, "/");
-            String shortName = tokenizer.nextToken();
-            Integer revision = Integer.parseInt(tokenizer.nextToken());
-            for (RemoteOntologyRevisions revisions: server.getOntologyList()) {
-                if (revisions.getShortName().equals(shortName)) {
-                    InputStream in = server.getOntologyStream(revisions.getOntologyName(), revision);
-                    in = new BufferedInputStream(in);
-                    OutputStream out = new BufferedOutputStream(response.getOutputStream());
-                    int c;
-                    while ((c = in.read()) != -1) {
-                        out.write(c);
-                    }
-                    out.flush();
-                    out.close();
-                    return;
-                }
-            }
-        }
-        catch (RemoteOntologyCreationException e) {
-            IOException ioe = new IOException(e.getMessage());
-            ioe.initCause(e);
-            throw ioe;
-        }
+    	String encoded = request.getPathInfo();
+    	StringTokenizer tokenizer = new StringTokenizer(encoded, "/");
+    	String shortName = tokenizer.nextToken();
+    	Integer revision = Integer.parseInt(tokenizer.nextToken());
+    	for (RemoteOntologyRevisions revisions: server.getOntologyList()) {
+    		if (revisions.getShortName().equals(shortName)) {
+    			InputStream in = server.getOntologyStream(revisions.getOntologyName(), revision);
+    			in = new BufferedInputStream(in);
+    			OutputStream out = new BufferedOutputStream(response.getOutputStream());
+    			int c;
+    			while ((c = in.read()) != -1) {
+    				out.write(c);
+    			}
+    			out.flush();
+    			out.close();
+    			return;
+    		}
+    	}
     }
 }
