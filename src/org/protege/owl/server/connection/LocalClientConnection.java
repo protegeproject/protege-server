034e8d420c48584384768b62fdc764234d15b1de
Timothy Redmond <tredmond@stanford.edu>
2010-04-17 05:44:31 +0000

    filling the pieces and making the AbstractClientConnection do all the possible common work

--- src/org/protege/owl/server/connection/LocalClientConnection.java
+++ src/org/protege/owl/server/connection/LocalClientConnection.java
@@ -1,116 +1,84 @@
 package org.protege.owl.server.connection;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.protege.owl.server.api.RemoteOntologyRevisions;
 import org.protege.owl.server.api.Server;
 import org.protege.owl.server.exception.RemoteOntologyChangeException;
-import org.protege.owl.server.exception.RemoteOntologyCreationException;
 import org.protege.owl.server.exception.RemoteOntologyException;
-import org.protege.owl.server.exception.UpdateFailedException;
+import org.protege.owl.server.exception.RemoteQueryException;
 import org.protege.owl.server.util.AbstractClientConnection;
 import org.protege.owlapi.apibinding.ProtegeOWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyChangeException;
+import org.semanticweb.owlapi.model.OWLOntologyChange;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public class LocalClientConnection extends AbstractClientConnection {
     private Server server;
-    private Map<IRI, Integer> localVersions = new HashMap<IRI,Integer>();
     
     public LocalClientConnection(Server server) {
         super(ProtegeOWLManager.createOWLOntologyManager());
         this.server = server;
     }
 
-    public void commit(OWLOntology ontology) throws RemoteOntologyChangeException {
-        IRI ontologyName = ontology.getOntologyID().getOntologyIRI();
-        if (!localVersions.containsKey(ontologyName)) {
-            throw new IllegalStateException("Ontology should be contained on client");
-        }
-        server.applyChanges(localVersions, getUncommittedChanges(ontology));
-        clearUncommittedChanges(ontology);
-    }
 
-    public Set<RemoteOntologyRevisions> getRemoteOntologyList() {
+
+    /* *****************************************************************************
+     * Abstract methods from AbstractClientConnection.
+     */
+    
+    @Override
+    protected Set<RemoteOntologyRevisions> updateRemoteOntologyList() throws RemoteQueryException {
         return server.getOntologyList();
     }
     
-    public Set<RemoteOntologyRevisions> updateRemoteOntologyList() {
-    	return server.getOntologyList();
-    }
-
-    public int getRevision(OWLOntology ontology) {
-        return localVersions.get(ontology.getOntologyID().getOntologyIRI());
-    }
-
-    public OWLOntology pull(IRI ontologyName, Integer revision) throws RemoteOntologyCreationException {
-        Set<RemoteOntologyRevisions> ontologyList = getRemoteOntologyList();
-        RemoteOntologyRevisions versions = null;
-        for (RemoteOntologyRevisions tryMe : ontologyList) {
-            if (tryMe.getOntologyName().equals(ontologyName)) {
-                versions = tryMe;
-                ontologyName = versions.getOntologyName();
-                break;
-            }
-        }
-        if (versions == null) { 
-            return null;
-        }
-        Integer closestRevision = versions.getLatestMarkedRevision(revision);
-        if (closestRevision == null) {
-            return null;
-        }
-        OWLOntology ontology = null; 
+    @Override
+    public OWLOntology pullMarked(IRI ontologyName, String shortName, int revision) throws OWLOntologyCreationException, RemoteQueryException  {
         try {
-            ontology = getOntologyManager().loadOntologyFromOntologyDocument(server.getOntologyStream(ontologyName, closestRevision));
-            getOntologyManager().applyChanges(server.getChanges(ontologyName, closestRevision, revision));
-            return ontology;
+            return getOntologyManager().loadOntologyFromOntologyDocument(server.getOntologyStream(ontologyName, revision));
         }
-        catch (RemoteOntologyCreationException e) {
+        catch (IOException ioe) {
+            throw new RemoteQueryException(ioe);
+        }
+    }
+    
+    @Override
+    protected List<OWLOntologyChange> getChangesFromServer(OWLOntology ontology, String shortName, int start, int end) throws RemoteQueryException {
+        try {
+            return server.getChanges(ontology.getOntologyID().getOntologyIRI(), start, end);
+        }
+        catch (RemoteQueryException e) {
             throw e;
         }
-        catch (OWLOntologyCreationException e) {
-            throw new RemoteOntologyCreationException(e);
-        }
         catch (RemoteOntologyException e) {
-            throw new RemoteOntologyCreationException(e);
+            throw new RemoteQueryException(e);
+        }
+    }
+    
+    /* *****************************************************************************
+     * Interface implementations.
+     */
+
+    
+    @Override
+    public void commit(Set<OWLOntology> ontologies) throws RemoteOntologyChangeException {
+        List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+        Map<IRI, Integer> versionMap = new HashMap<IRI, Integer>();
+        for (OWLOntology ontology : ontologies) {
+            IRI ontologyName = ontology.getOntologyID().getOntologyIRI();
+            changes.addAll(getUncommittedChanges(ontology));
+            versionMap.put(ontologyName, getRevision(ontology));
+        }
+        server.applyChanges(versionMap, changes);
+        for (OWLOntology ontology : ontologies) {
+            clearUncommittedChanges(ontology);
         }
     }
 
-    public void update(OWLOntology ontology, Integer revision) throws OWLOntologyChangeException {
-        IRI ontologyName = ontology.getOntologyID().getOntologyIRI();
-        Integer currentRevision = localVersions.get(ontologyName);
-        if (currentRevision == null) {
-            return;
-        }
-        if (revision == null) {
-            Set<RemoteOntologyRevisions> ontologyList = getRemoteOntologyList();
-            RemoteOntologyRevisions revisions = null;
-            for (RemoteOntologyRevisions tryMe : ontologyList) {
-                if (tryMe.getOntologyName().equals(ontologyName)) {
-                    revisions = tryMe;
-                }
-            }
-            revision = revisions.getMaxRevision();
-        }
-        try {
-            getOntologyManager().applyChanges(server.getChanges(ontologyName, currentRevision, revision));
-        }
-        catch (RemoteOntologyException e) {
-            throw new UpdateFailedException(e);
-        }
-    }
-
-    public Set<OWLOntology> getOntologies() {
-        Set<OWLOntology> ontologies = new HashSet<OWLOntology>();
-        for (IRI ontologyName : localVersions.keySet()) {
-            ontologies.add(getOntologyManager().getOntology(ontologyName));
-        }
-        return ontologies;
-    }
 }
