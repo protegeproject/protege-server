24fed948882be388c6b9fe0994eb42ba31a0d07e
Timothy Redmond <tredmond@stanford.edu>
2010-04-19 05:31:11 +0000

    added an annotation describing what aparticular ontology a particular change should go to.

--- src/org/protege/owl/server/util/ChangeToAxiomConverter.java
+++ src/org/protege/owl/server/util/ChangeToAxiomConverter.java
@@ -1,17 +1,20 @@
 package org.protege.owl.server.util;
 
-
 import static org.protege.owl.server.util.OntologyConstants.ADD_AXIOM_ANNOTATION;
+import static org.protege.owl.server.util.OntologyConstants.AXIOM_ABOUT;
 import static org.protege.owl.server.util.OntologyConstants.NS;
+import static org.protege.owl.server.util.OntologyConstants.ONTOLOGY_CURRENT_REVISION_PROPERTY;
 import static org.protege.owl.server.util.OntologyConstants.ONTOLOGY_IMPORTS;
+import static org.protege.owl.server.util.OntologyConstants.ONTOLOGY_NAME_PROPERTY;
 import static org.protege.owl.server.util.OntologyConstants.REMOTE_ONTOLOGY_CLASS;
-import static org.protege.owl.server.util.OntologyConstants.REMOTE_ONTOLOGY_INSTANCE;
 import static org.protege.owl.server.util.OntologyConstants.REMOVE_AXIOM_ANNOTATION;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -20,6 +23,7 @@ import org.semanticweb.owlapi.model.AddImport;
 import org.semanticweb.owlapi.model.AddOntologyAnnotation;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -34,10 +38,13 @@ import org.semanticweb.owlapi.model.RemoveOntologyAnnotation;
 import org.semanticweb.owlapi.model.SetOntologyID;
 
 public class ChangeToAxiomConverter implements OWLOntologyChangeVisitor {
+    private int counter = 0;
 	private OWLOntologyManager manager;
 	private OWLDataFactory factory;
-	private OWLOntology ontology;
+	private OWLOntology metaOntology;
 	private AddRemoveAxiomVisitor visitor = new AddRemoveAxiomVisitor();
+	private Map<IRI, OWLAnonymousIndividual> nameToRemoteOntologyInstanceMap = new HashMap<IRI, OWLAnonymousIndividual>();
+	
 	
 	public ChangeToAxiomConverter() throws OWLOntologyCreationException {
 		init();
@@ -46,32 +53,52 @@ public class ChangeToAxiomConverter implements OWLOntologyChangeVisitor {
 	public void init() throws OWLOntologyCreationException {
 		manager = OWLManager.createOWLOntologyManager();
 		factory = manager.getOWLDataFactory();
-		ontology = manager.createOntology(IRI.create(NS));
+		metaOntology = manager.createOntology(IRI.create(NS));
 		
 		List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
 		OWLAxiom decl = factory.getOWLDeclarationAxiom(REMOTE_ONTOLOGY_CLASS);
-		changes.add(new AddAxiom(ontology, decl));
-		decl = factory.getOWLClassAssertionAxiom(REMOTE_ONTOLOGY_CLASS, REMOTE_ONTOLOGY_INSTANCE);
-		changes.add(new AddAxiom(ontology, decl));
+		changes.add(new AddAxiom(metaOntology, decl));
 		manager.applyChanges(changes);
 	}
 	
-	public OWLOntology getOntology() {
-		return ontology;
+	public OWLAnonymousIndividual getOntologyDecl(OWLOntology realOntology) {
+	    IRI ontologyName = realOntology.getOntologyID().getOntologyIRI();
+	    OWLAnonymousIndividual o = nameToRemoteOntologyInstanceMap.get(ontologyName);
+	    if (o == null) {
+	        o = factory.getOWLAnonymousIndividual();
+	        List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+	        changes.add(new AddAxiom(metaOntology, factory.getOWLClassAssertionAxiom(REMOTE_ONTOLOGY_CLASS, o)));
+	        changes.add(new AddAxiom(metaOntology, 
+	                                 factory.getOWLDataPropertyAssertionAxiom(ONTOLOGY_NAME_PROPERTY, o, factory.getOWLTypedLiteral(ontologyName.toString()))));
+	        manager.applyChanges(changes);
+	        nameToRemoteOntologyInstanceMap.put(ontologyName, o);
+	    }
+	    return o;
+	}
+	
+	public void addRevisionInfo(OWLOntology realOntology, int revision) {
+	    OWLAnonymousIndividual o = getOntologyDecl(realOntology);
+        manager.addAxiom(metaOntology, factory.getOWLDataPropertyAssertionAxiom(ONTOLOGY_CURRENT_REVISION_PROPERTY, o, factory.getOWLTypedLiteral(revision)));
+	}
+	
+	public OWLOntology getMetaOntology() {
+		return metaOntology;
 	}
 
 	
 	public void visit(AddAxiom change) {
 		visitor.setAddAxiom(true);
+		visitor.setOntology(change.getOntology());
 		change.getAxiom().accept(visitor);
-		manager.addAxiom(ontology, visitor.getOWLAxiom());
+		manager.addAxiom(metaOntology, visitor.getOWLAxiom());
 	}
 
 	
 	public void visit(RemoveAxiom change) {
 		visitor.setAddAxiom(false);
+		visitor.setOntology(change.getOntology());
 		change.getAxiom().accept(visitor);
-		manager.addAxiom(ontology, visitor.getOWLAxiom());
+		manager.addAxiom(metaOntology, visitor.getOWLAxiom());
 	}
 
 	
@@ -81,22 +108,28 @@ public class ChangeToAxiomConverter implements OWLOntologyChangeVisitor {
 
 	
 	public void visit(AddImport change) {
-		OWLStringLiteral importAsLiteral = factory.getOWLStringLiteral(change.getImportDeclaration().getIRI().toString());
+	    OWLStringLiteral importAsLiteral = factory.getOWLStringLiteral(change.getImportDeclaration().getIRI().toString());
+	    Set<OWLAnnotation> importAnnotations = new HashSet<OWLAnnotation>();
+	    importAnnotations.add(REMOVE_AXIOM_ANNOTATION);
+	    importAnnotations.add(getAboutAnnotation(change.getOntology()));
 		OWLAxiom doImport = factory.getOWLDataPropertyAssertionAxiom(ONTOLOGY_IMPORTS, 
-																	 REMOTE_ONTOLOGY_INSTANCE, 
+																	 getOntologyDecl(change.getOntology()), 
 																	 importAsLiteral,
-																	 Collections.singleton(ADD_AXIOM_ANNOTATION));
-		manager.addAxiom(ontology, doImport);
+																	 importAnnotations);
+		manager.addAxiom(metaOntology, doImport);
 	}
 
 	
 	public void visit(RemoveImport change) {
 		OWLStringLiteral importAsLiteral = factory.getOWLStringLiteral(change.getImportDeclaration().getIRI().toString());
+		Set<OWLAnnotation> importAnnotations = new HashSet<OWLAnnotation>();
+		importAnnotations.add(REMOVE_AXIOM_ANNOTATION);
+		importAnnotations.add(getAboutAnnotation(change.getOntology()));
 		OWLAxiom removeImport = factory.getOWLDataPropertyAssertionAxiom(ONTOLOGY_IMPORTS, 
-																		 REMOTE_ONTOLOGY_INSTANCE, 
+		                                                                 getOntologyDecl(change.getOntology()), 
 																		 importAsLiteral,
-																		 Collections.singleton(REMOVE_AXIOM_ANNOTATION));
-		manager.addAxiom(ontology, removeImport);	
+																		 importAnnotations);
+		manager.addAxiom(metaOntology, removeImport);	
 	}
 
 	
@@ -104,10 +137,11 @@ public class ChangeToAxiomConverter implements OWLOntologyChangeVisitor {
 		OWLAnnotation annotation = change.getAnnotation();
 		Set<OWLAnnotation> annotationAnnotations = new HashSet<OWLAnnotation>(annotation.getAnnotations());
 		annotationAnnotations.add(ADD_AXIOM_ANNOTATION);
-		OWLAxiom axiom = factory.getOWLAnnotationAssertionAxiom(REMOTE_ONTOLOGY_INSTANCE.getIRI(),
+		annotationAnnotations.add(getAboutAnnotation(change.getOntology()));
+		OWLAxiom axiom = factory.getOWLAnnotationAssertionAxiom(getOntologyDecl(change.getOntology()),
 																annotation,
 																annotationAnnotations);
-		manager.addAxiom(ontology, axiom);
+		manager.addAxiom(metaOntology, axiom);
 	}
 
 	
@@ -115,28 +149,34 @@ public class ChangeToAxiomConverter implements OWLOntologyChangeVisitor {
 		OWLAnnotation annotation = change.getAnnotation();
 		Set<OWLAnnotation> annotationAnnotations = new HashSet<OWLAnnotation>(annotation.getAnnotations());
 		annotationAnnotations.add(REMOVE_AXIOM_ANNOTATION);
-		OWLAxiom axiom = factory.getOWLAnnotationAssertionAxiom(REMOTE_ONTOLOGY_INSTANCE.getIRI(),
+		annotationAnnotations.add(getAboutAnnotation(change.getOntology()));
+		OWLAxiom axiom = factory.getOWLAnnotationAssertionAxiom(getOntologyDecl(change.getOntology()),
 																annotation,
 																annotationAnnotations);
-		manager.addAxiom(ontology, axiom);
+		manager.addAxiom(metaOntology, axiom);
+	}
+	
+	private OWLAnnotation getAboutAnnotation(OWLOntology ontology) {
+	    return factory.getOWLAnnotation(AXIOM_ABOUT, getOntologyDecl(ontology));
 	}
 	
 	
 	private class AddRemoveAxiomVisitor extends AxiomAnnotationsVisitor {
 		private boolean addAxiom;
+		private OWLOntology ontology;
 
 		public void setAddAxiom(boolean addAxiom) {
 			this.addAxiom = addAxiom;
 		}
 		
-		public boolean isAddAxiom() {
-			return addAxiom;
+		public void setOntology(OWLOntology ontology) {
+		    this.ontology = ontology;
 		}
-		
 		
 		protected Set<OWLAnnotation> processAnnotations(Set<OWLAnnotation> axiomAnnotations) {
 			Set<OWLAnnotation> newAxiomAnnotations = new HashSet<OWLAnnotation>(axiomAnnotations);
 			newAxiomAnnotations.add(addAxiom ? ADD_AXIOM_ANNOTATION : REMOVE_AXIOM_ANNOTATION);
+			newAxiomAnnotations.add(getAboutAnnotation(ontology));
 			return newAxiomAnnotations;
 		}
 	}
