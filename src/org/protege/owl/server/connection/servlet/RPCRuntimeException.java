86ad81d9878bc7d29ef186d3ed0ca083cad576c5
Timothy Redmond <tredmond@stanford.edu>
2010-04-19 05:25:00 +0000

    nobody uses this

--- src/org/protege/owl/server/connection/servlet/RPCRuntimeException.java
+++
@@ -1,9 +0,0 @@
-package org.protege.owl.server.connection.servlet;
-
-public class RPCRuntimeException extends RuntimeException {
-    private static final long serialVersionUID = -5626805489265150643L;
-
-    public RPCRuntimeException(Throwable t) {
-        super(t);
-    }
-}
