913e548bd8791a649020d253a033c44813e9b7f9
Timothy Redmond <tredmond@stanford.edu>
2010-04-19 05:24:25 +0000

    added the commit implementation (tested\!) and fixed some null pointer issues

--- src/org/protege/owl/server/connection/servlet/ServletClientConnection.java
+++ src/org/protege/owl/server/connection/servlet/ServletClientConnection.java
@@ -7,20 +7,23 @@ import static org.protege.owl.server.util.OntologyConstants.ONTOLOGY_SHORT_NAME_
 import static org.protege.owl.server.util.OntologyConstants.REMOTE_ONTOLOGY_CLASS;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.protege.owl.server.api.RemoteOntologyRevisions;
+import org.protege.owl.server.api.ServerOntologyInfo;
 import org.protege.owl.server.connection.servlet.serialize.Serializer;
 import org.protege.owl.server.connection.servlet.serialize.SerializerFactory;
 import org.protege.owl.server.exception.OntologyConflictException;
 import org.protege.owl.server.exception.RemoteQueryException;
 import org.protege.owl.server.util.AbstractClientConnection;
 import org.protege.owl.server.util.AxiomToChangeConverter;
+import org.protege.owl.server.util.ChangeToAxiomConverter;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
@@ -48,8 +51,8 @@ public class ServletClientConnection extends AbstractClientConnection {
      */
     
 	@Override
-    protected Set<RemoteOntologyRevisions> updateRemoteOntologyList() throws RemoteQueryException {
-        Set<RemoteOntologyRevisions> result = new HashSet<RemoteOntologyRevisions>();
+    protected Set<ServerOntologyInfo> updateRemoteOntologyList() throws RemoteQueryException {
+        Set<ServerOntologyInfo> result = new HashSet<ServerOntologyInfo>();
         try {
             OWLOntologyManager otherManager = OWLManager.createOWLOntologyManager();
             OWLOntology response = serializer.deserialize(otherManager, new URL(httpPrefix + OntologyListServlet.PATH));
@@ -61,7 +64,7 @@ public class ServletClientConnection extends AbstractClientConnection {
                     markedRevisions.add(Integer.parseInt(value.getLiteral()));
                 }
                 OWLLiteral maxRevision = i.getDataPropertyValues(ONTOLOGY_MAX_REVISION_PROPERTY, response).iterator().next();
-                result.add(new RemoteOntologyRevisions(IRI.create(ontologyName.getLiteral()), 
+                result.add(new ServerOntologyInfo(IRI.create(ontologyName.getLiteral()), 
                                                        shortName.getLiteral(), 
                                                        markedRevisions, 
                                                        Integer.parseInt(maxRevision.getLiteral())));
@@ -93,11 +96,14 @@ public class ServletClientConnection extends AbstractClientConnection {
         try {
             OWLOntologyManager otherManager = OWLManager.createOWLOntologyManager();
             OWLOntology changeOntology = serializer.deserialize(otherManager, new URL(httpPrefix + OntologyDeltaServlet.PATH + "/" + shortName + "/" + start + "/" + end));
-            AxiomToChangeConverter converter = new AxiomToChangeConverter(ontology);
+            AxiomToChangeConverter converter = new AxiomToChangeConverter(changeOntology, Collections.singleton(ontology));
             List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
             for (OWLAxiom changeAxiom : changeOntology.getAxioms()) {
                 changeAxiom.accept(converter);
-                changes.add(converter.getChange());
+                OWLOntologyChange change = converter.getChange();
+                if (change != null) {
+                    changes.add(change);
+                }
             }
             return changes;
         }
@@ -114,7 +120,30 @@ public class ServletClientConnection extends AbstractClientConnection {
      */
 
 	@Override
-	public void commit(Set<OWLOntology> ontologies) throws OntologyConflictException {
-	    throw new UnsupportedOperationException("Not implemented yet");
+	public void commit(Set<OWLOntology> ontologies) throws OntologyConflictException, RemoteQueryException {
+	    try {
+	        ChangeToAxiomConverter converter = new ChangeToAxiomConverter();
+	        for (OWLOntology ontology : ontologies) {
+	            converter.addRevisionInfo(ontology, getRevision(ontology));
+	        }
+	        for (OWLOntologyChange change : getUncommittedChanges(ontologies)) {
+	            change.accept(converter);
+	        }
+	        URL servlet = new URL(httpPrefix + OntologyCommitServlet.PATH);
+	        URLConnection connection = servlet.openConnection();
+	        connection.setDoOutput(true);
+	        connection.connect();
+	        OWLOntology metaOntology = converter.getMetaOntology();
+	        serializer.serialize(metaOntology, connection.getOutputStream());
+	        if (((HttpURLConnection) connection).getResponseCode() != HttpURLConnection.HTTP_CONFLICT) {
+	            connection.getInputStream().close();
+	        }
+	        else {
+	            throw new UnsupportedOperationException("Not implemented yet");
+	        }
+	    }
+	    catch (Exception e) {
+	        throw new RemoteQueryException(e);
+	    }
 	}
 }
