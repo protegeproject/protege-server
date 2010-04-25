93e3fb5acbb21ac3448bc3fe893b8938ba64cccc
Timothy Redmond <tredmond@stanford.edu>
2010-04-25 18:18:26 +0000

    refactored the getOntologyInfo client and server methods

--- src/org/protege/owl/server/api/ClientConnection.java
+++ src/org/protege/owl/server/api/ClientConnection.java
@@ -1,6 +1,7 @@
 package org.protege.owl.server.api;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.protege.owl.server.exception.RemoteOntologyChangeException;
@@ -17,12 +18,23 @@ public interface ClientConnection extends OWLOntologySetProvider {
     
     OWLOntologyManager getOntologyManager();
     
-    Set<ServerOntologyInfo> getRemoteOntologyList(boolean forceUpdate) throws RemoteQueryException;
+    Map<IRI, ServerOntologyInfo> getOntologyInfoByIRI(boolean forceUpdate) throws RemoteQueryException;
+    
+    Map<String, ServerOntologyInfo> getOntologyInfoByShortName(boolean forceUpdate) throws RemoteQueryException;
     
     OWLOntology pull(IRI ontologyName, Integer revision) throws OWLOntologyCreationException, RemoteQueryException;
     
     int getRevision(OWLOntology ontology);
     
+    /**
+     * This call takes the set of local (client-side) changes made to the set of ontologies and commits them on the server.
+     * In addition, a consequence of making this call is that the client is brought up to date with the state of the server 
+     * at the point the changes were committed.
+     * 
+     * @param ontologies the ontologies with local changes that are to be committed to the server.
+     * @throws RemoteOntologyChangeException
+     * @throws RemoteQueryException
+     */
     void commit(Set<OWLOntology> ontologies) throws RemoteOntologyChangeException, RemoteQueryException;
     
     void update(OWLOntology ontology, Integer revision) throws OWLOntologyChangeException, RemoteQueryException;
