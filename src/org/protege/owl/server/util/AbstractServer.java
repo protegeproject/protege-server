890532a88c2901128d0387b0840374914509f722
Timothy Redmond <tredmond@stanford.edu>
2010-04-25 18:19:35 +0000

    added revision update on commit and moved som stuff for other users

--- src/org/protege/owl/server/util/AbstractServer.java
+++ src/org/protege/owl/server/util/AbstractServer.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.protege.owl.server.api.ConflictManager;
 import org.protege.owl.server.api.Server;
+import org.protege.owl.server.api.ServerOntologyInfo;
 import org.protege.owl.server.connection.LocalClientConnection;
 import org.protege.owl.server.exception.OntologyConflictException;
 import org.semanticweb.owlapi.io.RDFXMLOntologyFormat;
@@ -23,7 +25,6 @@ import org.semanticweb.owlapi.model.OWLImportsDeclaration;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 import org.semanticweb.owlapi.model.OWLOntologyChangeVisitor;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
@@ -35,6 +36,8 @@ import org.semanticweb.owlapi.model.SetOntologyID;
 public abstract class AbstractServer implements Server {
     private OWLOntologyManager ontologyManager;
     private ConflictManager conflictManager;
+    private Map<IRI, ServerOntologyInfo> ontologyInfoByIRI = new HashMap<IRI, ServerOntologyInfo>();
+    private Map<String, ServerOntologyInfo> ontologyInfoByShortName = new HashMap<String, ServerOntologyInfo>();
     
     protected AbstractServer(OWLOntologyManager ontologyManager) {
         this.ontologyManager = ontologyManager;
@@ -43,6 +46,7 @@ public abstract class AbstractServer implements Server {
     protected abstract boolean contains(IRI ontologyName, int revision, OWLAxiom axiom);
     protected abstract boolean contains(IRI ontologyName, int revision, OWLImportsDeclaration owlImport);
     protected abstract boolean contains(IRI ontologyName, int revision, OWLAnnotation annotation);
+    protected abstract int getUpdatedRevision(OWLOntology ontology);
     
     public OWLOntologyManager getOntologyManager() {
         return ontologyManager;
@@ -56,6 +60,26 @@ public abstract class AbstractServer implements Server {
         this.conflictManager = conflictManager;
     }
     
+    @Override
+    public Map<IRI, ServerOntologyInfo> getOntologyInfoByIRI() {
+        return new HashMap<IRI, ServerOntologyInfo>(ontologyInfoByIRI);
+    }
+
+    @Override
+    public Map<String, ServerOntologyInfo> getOntologyInfoByShortName() {
+        return new HashMap<String, ServerOntologyInfo>(ontologyInfoByShortName);
+    }
+    
+    protected void addOntologyInfo(ServerOntologyInfo info) {
+        ontologyInfoByIRI.put(info.getOntologyName(), info);
+        ontologyInfoByShortName.put(info.getShortName(), info);
+    }
+    
+    protected void removeOntologyInfo(ServerOntologyInfo info) {
+        ontologyInfoByIRI.remove(info.getOntologyName());
+        ontologyInfoByShortName.remove(info.getShortName());
+    }
+    
     
     public synchronized void applyChanges(Map<IRI, Integer> versions, List<OWLOntologyChange> changes) throws OntologyConflictException {
         changes = reduceChangeList(versions, changes);
@@ -63,6 +87,13 @@ public abstract class AbstractServer implements Server {
             conflictManager.validateChanges(versions, changes);
         }
         ontologyManager.applyChanges(changes);
+        for (OWLOntology ontology : Utilities.getChangedOntologies(changes)) {
+            IRI ontologyName = ontology.getOntologyID().getOntologyIRI();
+            ServerOntologyInfo original = getOntologyInfoByIRI().get(ontologyName);
+            int newRevision = getUpdatedRevision(ontology);
+            ServerOntologyInfo updated = new ServerOntologyInfo(ontologyName, original.getShortName(), original.getMarkedRevisions(), newRevision);
+            addOntologyInfo(updated);
+        }
     }
     
     public void save(OWLOntologyID id, int revision, File location) throws IOException, OWLOntologyStorageException {
