5cd5e83a613c84b61b4a8178fd6c0f7d48f13803
Timothy Redmond <tredmond@stanford.edu>
2010-08-02 23:51:13 +0000

    refactors to use the protege-owl-extensions.jar library

--- OSGi/run_server.sh
+++ OSGi/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Dlog4j.configuration=file:log4j.xml -Dfelix.config.properties=file:conf/server-config.properties -jar bin/felix.jar
+java -Dlog4j.configuration=file:log4j-server.xml -Dfelix.config.properties=file:conf/server-config.properties -jar bin/felix.jar
