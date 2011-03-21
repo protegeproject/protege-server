c13fe176bf6367732ebd2d469c69bdea73c85d4e
Timothy Redmond <tredmond@stanford.edu>
2011-03-21 00:52:49 +0000

    the run_server will run now

--- OSGi/run_server.sh
+++ OSGi/run_server.sh
@@ -3,4 +3,4 @@
 java -classpath bin/felix.jar:bin/ProtegeLauncher.jar \
      -Dorg.protege.launch.config=server-config.xml \
      -Dlog4j.configuration=file:log4j.xml \
-     org.protege.osgi.framework.Launcher  /home/tredmond/dev/workspaces/server/org.protege.owl.server/sample-metaproject.owl
+     org.protege.osgi.framework.Launcher sample-metaproject.owl
