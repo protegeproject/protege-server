15bcb8155c21a6d0b4b914ae5a18dbef93f43cfb
Timothy Redmond <tredmond@stanford.edu>
2011-03-21 00:35:04 +0000

    added an activator that sets the metaproject

---
+++ src/main/java/org/protege/owl/server/Activator.java
@@ -0,0 +1,70 @@
+package org.protege.owl.server;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.protege.owl.server.api.ConfigurationManager;
+import org.protege.owl.server.metaproject.MetaProject;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+public class Activator implements BundleActivator {
+    private Logger LOGGER = Logger.getLogger(Activator.class);
+    private BundleContext context;
+    private String metaproject;
+    private ServiceListener listener = new ServiceListener() {
+        public void serviceChanged(ServiceEvent event) {
+            if (loadCommandLineMetaproject()) {
+                context.removeServiceListener(listener);
+            }
+        }
+    };
+
+
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        this.context = context;
+        final String metaproject = System.getProperty("command.line.arg.0");
+        this.metaproject = metaproject;
+        if (metaproject != null) {
+            if (!loadCommandLineMetaproject()) {
+                context.addServiceListener(listener);
+            }
+        }
+    }
+    
+    
+    
+    private boolean loadCommandLineMetaproject() {
+        try {
+            ServiceReference sr = context.getServiceReference(ConfigurationManager.class.getCanonicalName());
+            if (sr != null) {
+                ConfigurationManager configManager = (ConfigurationManager) context.getService(sr);
+                OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+                MetaProject.addMetaProjectIRIMapper(manager);
+                OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(metaproject));
+                configManager.setMetaOntology(ontology);
+                return true;
+            }
+        }
+        catch (Exception e) {
+            LOGGER.error("Exception caught trying to load metaproject: " + metaproject, e);
+        }
+        return false;
+    }
+    
+
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+    }
+
+}
