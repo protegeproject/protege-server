cc14bfd2725e1611b2cd6f3ba5d95e8e9b44e01e
Timothy Redmond <tredmond@stanford.edu>
2012-07-09 16:22:34 +0000

    The first factory - will contain some of the core server components.

---
+++ src/main/java/org/protege/owl/server/configuration/CoreServerFactory.java
@@ -0,0 +1,52 @@
+package org.protege.owl.server.configuration;
+
+import org.protege.owl.server.api.Server;
+import org.protege.owl.server.api.ServerComponentFactory;
+import org.protege.owl.server.api.ServerFilter;
+import org.protege.owl.server.api.ServerTransport;
+import org.semanticweb.owlapi.model.OWLIndividual;
+
+public class CoreServerFactory implements ServerComponentFactory {
+
+	@Override
+	public boolean hasSuitableServer(OWLIndividual i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Server createServer(OWLIndividual i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasSuitableServerFilter(OWLIndividual i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ServerFilter createServerFilter(OWLIndividual i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServerTransport hasSuitableServerTransport(OWLIndividual i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServerTransport createServerTransport(OWLIndividual i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "<<Core Server Components>>";
+	}
+
+}
