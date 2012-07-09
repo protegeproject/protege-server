0498fd65d11679a4f7a362bdb1265dacf83aef52
Timothy Redmond <tredmond@stanford.edu>
2012-07-09 16:20:57 +0000

    Made a builder interface for the OSGi declarative services.

---
+++ src/main/java/org/protege/owl/server/api/Builder.java
@@ -0,0 +1,7 @@
+package org.protege.owl.server.api;
+
+public interface Builder {
+	 void addServerComponentFactory(ServerComponentFactory factory);
+	 
+	 void removeServerComponentFactory(ServerComponentFactory factory);
+}
