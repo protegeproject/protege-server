51b8d64722411c68590c645a25ad7d3789f0be97
Timothy Redmond <tredmond@stanford.edu>
2012-07-26 13:27:42 +0000

    added some explanation for the future when Matthew's change serialization is committed

--- src/main/java/org/protege/owl/server/util/LazyCroppedChangeDocument.java
+++ src/main/java/org/protege/owl/server/util/LazyCroppedChangeDocument.java
@@ -9,6 +9,16 @@ import org.protege.owl.server.api.OntologyDocumentRevision;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
+/**
+ * When Matthew's change serialization code comes, this can be optimized.  It will then be possible to 
+ * crop the changes of a large history file without reading all of the changes from the history file in.
+ * This will spare the client the effort of loading an entire history document when he only wants a small section.
+ * At that time this class will probably need to move from the util package to the changes or impl package.
+ * 
+ * @author redmond
+ * @deprecated
+ */
+@Deprecated
 public class LazyCroppedChangeDocument implements ChangeDocument {
 	private static final long serialVersionUID = -7116780557239070763L;
 	private ChangeDocument allChanges;
