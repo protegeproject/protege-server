3b3fb7a306b73a5283a33105d1aefda4e75ead80
Timothy Redmond <tredmond@stanford.edu>
2012-08-01 12:32:31 +0000

    The document factory implementation was moved.

--- src/test/java/org/protege/owl/server/changes/BasicSerializationTest.java
+++ src/test/java/org/protege/owl/server/changes/BasicSerializationTest.java
@@ -1,7 +1,6 @@
 package org.protege.owl.server.changes;
 
 import org.protege.owl.server.api.DocumentFactory;
-import org.protege.owl.server.impl.DocumentFactoryImpl;
 
 public class BasicSerializationTest extends AbstractSerializationTest {
 
