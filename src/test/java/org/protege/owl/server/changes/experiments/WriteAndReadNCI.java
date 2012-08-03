825172026b685eec1fc9f1ecf2535384d37abdea
Timothy Redmond <tredmond@stanford.edu>
2012-08-03 21:04:42 +0000

    At least in a couple of tests the buffered read of a small section of the file was very fast.

--- src/test/java/org/protege/owl/server/changes/experiments/WriteAndReadNCI.java
+++ src/test/java/org/protege/owl/server/changes/experiments/WriteAndReadNCI.java
@@ -22,71 +22,82 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 public class WriteAndReadNCI {
-	public static final String ONTOLOGY_LOCATION = "/Users/tredmond/work/Shared/ontologies/NCI/Thesaurus-11.01e-fixed-annotations.owl";
-	
-	/*
+    public static final String ONTOLOGY_LOCATION = "/Users/tredmond/work/Shared/ontologies/NCI/Thesaurus-11.01e-fixed-annotations.owl";
+    
+    /*
      * Matthew's work should make this a couple of orders of magnitude faster!
      *
-	 * Loading ontology
-     * Writing history file
-     * Took 69705 ms.
-     * Take a deep breath...
-     * Reading file
-     * Took 50898 ms.
-     *
+     * Loading ontology
+     * 
      * With buffering...
-     * Writing history file
-     * Took 62781 ms.
-     * Take a deep breath...
-     * Reading file
-     * Took 173725 ms.
-	 */
-	public static void main(String[] args) throws Exception {
+     *     Writing history file
+     *     Took 65360 ms.
+     *     Take a deep breath...
+     *     Reading file
+     *     Took 36669 ms.
+     *     Reading a small section of the file
+     *     Took 1978 ms.
+     * Without buffering...
+     *     Writing history file
+     *     Took 64446 ms.
+     *     Take a deep breath...
+     *     Reading file
+     *     Took 45355 ms.
+     *     Reading a small section of the file
+     *     Took 45882 ms.
+     */
+    public static void main(String[] args) throws Exception {
         OWLOntology ontology = loadOntology();
-        DocumentFactoryImpl factory1 = new DocumentFactoryImpl();
-	    File f1 = writeAxioms(ontology, factory1);
-		breath();
-		readAxioms(f1, factory1);
-		
-		System.out.println("\nWith buffering...");
-		DocumentFactory factory2 = new BufferedDocumentFactory(new DocumentFactoryImpl());
-		File f2 = writeAxioms(ontology, factory2);
-		breath();
-		readAxioms(f2, factory2);
-	}
-	
-	public static OWLOntology loadOntology() throws OWLOntologyCreationException {
-		System.out.println("Loading ontology");
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		return manager.loadOntologyFromOntologyDocument(new File(ONTOLOGY_LOCATION));
-	}
-	
-	public static File writeAxioms(OWLOntology ontology, DocumentFactory factory) throws IOException, OWLOntologyCreationException {
-		System.out.println("Writing history file");
-		File tmp = File.createTempFile("Thesaurus", ".history");
-		List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
-		for (OWLAxiom axiom : ontology.getAxioms()) {
-			changes.add(new AddAxiom(ontology, axiom));
-		}
-		long startTime = System.currentTimeMillis();
-		ChangeDocument doc = factory.createChangeDocument(changes, new TreeMap<OntologyDocumentRevision, ChangeMetaData>(),OntologyDocumentRevision.START_REVISION);
-		ChangeDocumentUtilities.writeChanges(doc, tmp);
-		System.out.println("Took " + (System.currentTimeMillis() - startTime) + " ms.");
 
-		return tmp;
-	}
-	
-	public static void breath() {
-		System.out.println("Take a deep breath...");
-		System.gc();
-		System.gc();
-		System.gc();
-	}
-	
-	public static void readAxioms(File tmp, DocumentFactory factory) throws IOException  {
-		System.out.println("Reading file");
-		long startTime = System.currentTimeMillis();
-		ChangeDocumentUtilities.readChanges(factory, tmp, null, null);
-		System.out.println("Took " + (System.currentTimeMillis() - startTime) + " ms.");
-	}
+        System.out.println("\nWith buffering...");
+        DocumentFactory factory2 = new BufferedDocumentFactory(new DocumentFactoryImpl());
+        File f2 = writeAxioms(ontology, factory2);
+        breath();
+        readAxioms(f2, factory2);
+        
+        System.out.println("Without buffering...");
+        DocumentFactoryImpl factory1 = new DocumentFactoryImpl();
+        File f1 = writeAxioms(ontology, factory1);
+        breath();
+        readAxioms(f1, factory1);
+    }
+    
+    public static OWLOntology loadOntology() throws OWLOntologyCreationException {
+        System.out.println("Loading ontology");
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        return manager.loadOntologyFromOntologyDocument(new File(ONTOLOGY_LOCATION));
+    }
+    
+    public static File writeAxioms(OWLOntology ontology, DocumentFactory factory) throws IOException, OWLOntologyCreationException {
+        System.out.println("\tWriting history file");
+        File tmp = File.createTempFile("Thesaurus", ".history");
+        List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+        for (OWLAxiom axiom : ontology.getAxioms()) {
+            changes.add(new AddAxiom(ontology, axiom));
+        }
+        long startTime = System.currentTimeMillis();
+        ChangeDocument doc = factory.createChangeDocument(changes, new TreeMap<OntologyDocumentRevision, ChangeMetaData>(),OntologyDocumentRevision.START_REVISION);
+        ChangeDocumentUtilities.writeChanges(doc, tmp);
+        System.out.println("\tTook " + (System.currentTimeMillis() - startTime) + " ms.");
+
+        return tmp;
+    }
+    
+    public static void breath() {
+        System.out.println("\tTake a deep breath...");
+        System.gc();
+        System.gc();
+        System.gc();
+    }
+    
+    public static void readAxioms(File tmp, DocumentFactory factory) throws IOException  {
+        System.out.println("\tReading file");
+        long startTime = System.currentTimeMillis();
+        ChangeDocumentUtilities.readChanges(factory, tmp, null, null);
+        System.out.println("\tTook " + (System.currentTimeMillis() - startTime) + " ms.");
+        System.out.println("\tReading a small section of the file");
+        startTime = System.currentTimeMillis();
+        ChangeDocumentUtilities.readChanges(factory, tmp, new OntologyDocumentRevision(12345), new OntologyDocumentRevision(13456));
+        System.out.println("\tTook " + (System.currentTimeMillis() - startTime) + " ms.");
+    }
 }
