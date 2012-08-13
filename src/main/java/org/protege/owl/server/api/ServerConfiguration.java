ad5f54d374ec597bc9ffc3190da1bfe83ba3d648
Timothy Redmond <tredmond@stanford.edu>
2012-08-13 20:24:34 +0000

    using declarative services to initialize the builder configuration

---
+++ src/main/java/org/protege/owl/server/api/ServerConfiguration.java
@@ -0,0 +1,9 @@
+package org.protege.owl.server.api;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public interface ServerConfiguration {
+    
+    public OWLOntology getMetaOntology();
+
+}
