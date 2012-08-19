618ffc2f5157fe1ec4adde3aae50bb10ec6c722d
Timothy Redmond <tredmond@stanford.edu>
2012-08-19 00:18:44 +0000

    divided the notion of a server document into two: server documents (which have no scheme or host) and remote documents (which have been given a scheme and host by transport)

---
+++ src/main/java/org/protege/owl/server/api/RemoteServerDocument.java
@@ -0,0 +1,9 @@
+package org.protege.owl.server.api;
+
+import org.semanticweb.owlapi.model.IRI;
+
+public interface RemoteServerDocument extends Comparable<RemoteServerDocument> {
+    ServerDocument createServerDocument();
+    
+    IRI getServerLocation();
+}
