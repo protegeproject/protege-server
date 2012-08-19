091114c20e7d2e587700ffc27ea7a21ad25f12d9
Timothy Redmond <tredmond@stanford.edu>
2012-08-19 00:19:29 +0000

    divided the notion of a server document into two: server documents (which have no scheme or host) and remote documents (which have been given a scheme and host by transport)

---
+++ src/main/java/org/protege/owl/server/api/RemoteOntologyDocument.java
@@ -0,0 +1,6 @@
+package org.protege.owl.server.api;
+
+public interface RemoteOntologyDocument extends RemoteServerDocument {
+
+    ServerOntologyDocument createServerDocument();
+}
---
+++ src/main/java/org/protege/owl/server/api/RemoteServerDirectory.java
@@ -0,0 +1,6 @@
+package org.protege.owl.server.api;
+
+public interface RemoteServerDirectory extends RemoteServerDocument {
+    
+    ServerDirectory createServerDocument();
+}
