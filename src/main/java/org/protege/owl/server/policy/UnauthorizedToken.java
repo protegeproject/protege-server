aadb678fc9a2eb52c2520431f9887176cf7efd62
Timothy Redmond <tredmond@stanford.edu>
2012-08-19 00:23:32 +0000

    a fake auth token for testing and when security is not relevant

---
+++ src/main/java/org/protege/owl/server/policy/UnauthorizedToken.java
@@ -0,0 +1,27 @@
+package org.protege.owl.server.policy;
+
+import java.io.Serializable;
+
+import org.protege.owl.server.api.AuthToken;
+import org.protege.owl.server.api.UserId;
+
+public class UnauthorizedToken implements AuthToken, Serializable {
+    private static final long serialVersionUID = 5535482187202979494L;
+    
+    private String userName;
+    
+    public UnauthorizedToken(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public int compareTo(AuthToken o) {
+        return getUserId().compareTo(o.getUserId());
+    }
+
+    @Override
+    public UserId getUserId() {
+        return new UserId(userName);
+    }
+
+}
