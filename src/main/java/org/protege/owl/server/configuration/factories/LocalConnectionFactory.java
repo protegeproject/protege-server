e396e5926cf21d9117f0c4caea0e3172ab2ddbec
Timothy Redmond <tredmond@stanford.edu>
2012-08-30 00:25:09 +0000

    starting work on the local transport for the wiki

--- src/main/java/org/protege/owl/server/configuration/factories/LocalConnectionFactory.java
+++ src/main/java/org/protege/owl/server/configuration/factories/LocalConnectionFactory.java
@@ -1,18 +1,23 @@
 package org.protege.owl.server.configuration.factories;
 
+import static org.protege.owl.server.configuration.MetaprojectVocabulary.LOCAL_TRANSPORT;
 import org.protege.owl.server.api.Server;
 import org.protege.owl.server.api.ServerComponentFactory;
 import org.protege.owl.server.api.ServerFilter;
 import org.protege.owl.server.api.ServerTransport;
+import org.protege.owl.server.connect.local.LocalTransportImpl;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 public class LocalConnectionFactory implements ServerComponentFactory {
     private OWLOntology ontology;
+    private OWLDataFactory factory;
 
     @Override
     public void setConfiguration(OWLOntology ontology) {
         this.ontology = ontology;
+        this.factory = ontology.getOWLOntologyManager().getOWLDataFactory();
     }
 
     @Override
@@ -37,12 +42,12 @@ public class LocalConnectionFactory implements ServerComponentFactory {
 
     @Override
     public boolean hasSuitableServerTransport(OWLIndividual i) {
-        return false;
+        return ontology.containsAxiom(factory.getOWLClassAssertionAxiom(LOCAL_TRANSPORT, i));
     }
 
     @Override
     public ServerTransport createServerTransport(OWLIndividual i) {
-        return null;
+        return new LocalTransportImpl();
     }
 
 }
