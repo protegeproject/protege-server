e708ead50278cce267803fd9b1bce3f6cd72c5dc
Timothy Redmond <tredmond@stanford.edu>
2012-09-07 01:50:29 +0000

    We now have a init.d script which will start but not shutdown the Protege Server.

--- etc/server.template/run.sh
+++ etc/server.template/run.sh
@@ -4,10 +4,10 @@ cd `dirname $0`
 
 java -Xmx3500M -Xms250M \
      -server \
-     -Djava.rmi.server.hostname=`hostname`
+     -Djava.rmi.server.hostname=`hostname` \
      -DentityExpansionLimit=100000000 \
      -Dfile.encoding=UTF-8 \
      -Dorg.protege.owl.server.configuration=metaproject.owl \
      -Djava.util.logging.config.file=logging.properties \
-     -classpath lib/felix.jar:lib/ProtegeLauncher.jar \
+     -classpath ./lib/felix.jar:./lib/ProtegeLauncher.jar \
      org.protege.osgi.framework.Launcher
