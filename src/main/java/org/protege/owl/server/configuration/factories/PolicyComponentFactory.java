e21fa25335c392d37d8acfda71ea01c30ff41c12
Timothy Redmond <tredmond@stanford.edu>
2012-10-02 00:09:53 +0000

    The policy factory now creates both authentication filters and authorization filters.

--- src/main/java/org/protege/owl/server/configuration/factories/PolicyComponentFactory.java
+++ src/main/java/org/protege/owl/server/configuration/factories/PolicyComponentFactory.java
@@ -1,10 +1,12 @@
 package org.protege.owl.server.configuration.factories;
 
 import static org.protege.owl.server.configuration.MetaprojectVocabulary.BASIC_AUTHENTICATION_MANAGER;
+import static org.protege.owl.server.configuration.MetaprojectVocabulary.BASIC_AUTHORIZATION_MANAGER;
 
 import org.protege.owl.server.api.Server;
 import org.protege.owl.server.api.ServerFilter;
 import org.protege.owl.server.policy.Authenticator;
+import org.protege.owl.server.policy.PolicyFilter;
 import org.protege.owl.server.util.ServerComponentFactoryAdapter;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
@@ -23,14 +25,31 @@ public class PolicyComponentFactory extends ServerComponentFactoryAdapter {
 
     @Override
     public boolean hasSuitableServerFilter(OWLIndividual i) {
+        return hasSuitableAuthenticationFilter(i) || hasSuitableAuthorizationFilter(i);
+    }
+    
+    private boolean hasSuitableAuthenticationFilter(OWLIndividual i) {
         OWLAxiom hasRightType = factory.getOWLClassAssertionAxiom(BASIC_AUTHENTICATION_MANAGER, i);
-        return metaOntology.containsAxiom(hasRightType);
+        return metaOntology.containsAxiom(hasRightType); 
+    }
+    
+    private boolean hasSuitableAuthorizationFilter(OWLIndividual i) {
+        OWLAxiom hasRightType = factory.getOWLClassAssertionAxiom(BASIC_AUTHORIZATION_MANAGER, i);
+        return metaOntology.containsAxiom(hasRightType); 
     }
 
     @Override
     public ServerFilter createServerFilter(OWLIndividual i, Server server) {
         try {
-            return new Authenticator(server);
+            if (hasSuitableAuthenticationFilter(i)) {
+                return new Authenticator(server);
+            }
+            else if (hasSuitableAuthorizationFilter(i)) {
+                return new PolicyFilter(server);
+            }
+            else {
+                throw new IllegalArgumentException("Invalid call to policy component factory.");
+            }
         }
         catch (Exception e) {
             throw new RuntimeException("Factory failed to setup server", e);
