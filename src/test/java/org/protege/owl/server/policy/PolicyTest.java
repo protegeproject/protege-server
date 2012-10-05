e86aa5fa7bdec138a5b9be98f665837ff8ff1d70
Timothy Redmond <tredmond@stanford.edu>
2012-10-05 13:15:52 +0000

    added several policy tests.

--- src/test/java/org/protege/owl/server/policy/PolicyTest.java
+++ src/test/java/org/protege/owl/server/policy/PolicyTest.java
@@ -3,6 +3,7 @@ package org.protege.owl.server.policy;
 import static org.protege.owl.server.PizzaVocabulary.CHEESEY_PIZZA_DEFINITION;
 import static org.protege.owl.server.TestUtilities.FERGERSON;
 import static org.protege.owl.server.TestUtilities.GUEST;
+import static org.protege.owl.server.TestUtilities.REDMOND;
 
 import java.io.IOException;
 import java.rmi.NotBoundException;
@@ -18,10 +19,13 @@ import org.protege.owl.server.api.ChangeMetaData;
 import org.protege.owl.server.api.Client;
 import org.protege.owl.server.api.OntologyDocumentRevision;
 import org.protege.owl.server.api.RemoteOntologyDocument;
+import org.protege.owl.server.api.RemoteServerDirectory;
+import org.protege.owl.server.api.RevisionPointer;
 import org.protege.owl.server.api.exception.AuthorizationFailedException;
 import org.protege.owl.server.api.exception.OWLServerException;
 import org.protege.owl.server.connect.rmi.RMIClient;
 import org.protege.owl.server.util.ClientUtilities;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -35,6 +39,10 @@ import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
 public class PolicyTest {
+    private IRI fergersonsPizzaLoc;
+    private IRI redmondsPrivateDirLoc;
+    private IRI redmondsPrivatePizzaLoc;
+
     private Framework framework;
     private int rmiPort;
     
@@ -42,6 +50,10 @@ public class PolicyTest {
     @Parameters({ "rmiPort" })
     public void setRMIPort(int rmiPort) {
         this.rmiPort = rmiPort;
+        fergersonsPizzaLoc = IRI.create(RMIClient.SCHEME + "://localhost:" + rmiPort + "/pizza-fergerson.history");
+        String redmondsPrivatePrefix = RMIClient.SCHEME + "://localhost:" + rmiPort + "/redmonds-private-directory";
+        redmondsPrivateDirLoc = IRI.create(redmondsPrivatePrefix);
+        redmondsPrivatePizzaLoc = IRI.create(redmondsPrivatePrefix + "/pizza.history");
     }
     
     @BeforeMethod
@@ -56,20 +68,80 @@ public class PolicyTest {
     }
     
     @Test
-    public void testCommitFailure() throws RemoteException, NotBoundException, OWLOntologyCreationException, OWLServerException {
-        IRI pizzaLocation = IRI.create(RMIClient.SCHEME + "://localhost:" + rmiPort + "/pizza-fergerson.history");
-        Client fergersonsClient = TestUtilities.createClient(rmiPort, FERGERSON);
-        OWLOntology pizza = PizzaVocabulary.loadPizza();
-        ClientUtilities.createServerOntology(fergersonsClient, pizzaLocation, new ChangeMetaData(), pizza);
+    public void testCantTraverseParentDir() throws OWLOntologyCreationException, NotBoundException, OWLServerException, RemoteException {
+        createRedmondsPrivateDir();
+        Client redmondsClient = TestUtilities.createClient(rmiPort, REDMOND);
+        RemoteOntologyDocument pizzaDoc = (RemoteOntologyDocument) redmondsClient.getServerDocument(redmondsPrivatePizzaLoc);
         Client guestClient = TestUtilities.createClient(rmiPort, GUEST);
-        RemoteOntologyDocument remotePizza = (RemoteOntologyDocument) guestClient.getServerDocument(pizzaLocation);
         boolean authorizationFailed = false;
         try {
-            TestUtilities.rawCommit(guestClient, remotePizza, new OntologyDocumentRevision(1), new RemoveAxiom(pizza, CHEESEY_PIZZA_DEFINITION));
+             guestClient.evaluateRevisionPointer(pizzaDoc, RevisionPointer.HEAD_REVISION);
         }
         catch (AuthorizationFailedException afe) {
             authorizationFailed = true;
         }
         Assert.assertTrue(authorizationFailed);
+    }
+    
+    @Test
+    public void testEvaluatePointerFailure() throws RemoteException, OWLOntologyCreationException, NotBoundException, OWLServerException {
+        createFergersonsPizza();
+        // use fergersons client to get the remote ontology document in case a future version of the policy prevents guest from getting it.
+        Client fergersonsClient = TestUtilities.createClient(rmiPort, FERGERSON);
+        RemoteOntologyDocument pizzaDoc = (RemoteOntologyDocument) fergersonsClient.getServerDocument(fergersonsPizzaLoc);
+        Client guestClient = TestUtilities.createClient(rmiPort, GUEST);
+        boolean authorizationFailed = false;
+        try {
+             guestClient.evaluateRevisionPointer(pizzaDoc, RevisionPointer.HEAD_REVISION);
+        }
+        catch (AuthorizationFailedException afe) {
+            authorizationFailed = true;
+        }
+        Assert.assertTrue(authorizationFailed);
+    }
+    
+    @Test
+    public void testListFailure() throws RemoteException, OWLOntologyCreationException, NotBoundException, OWLServerException {
+        createRedmondsPrivateDir();
+        Client redmondsClient = TestUtilities.createClient(rmiPort, REDMOND);
+        RemoteServerDirectory redmondsDir = (RemoteServerDirectory) redmondsClient.getServerDocument(redmondsPrivateDirLoc);
+        Client guestClient = TestUtilities.createClient(rmiPort, GUEST);
+        boolean authorizationFailed = false;
+        try {
+             guestClient.list(redmondsDir);
+        }
+        catch (AuthorizationFailedException afe) {
+            authorizationFailed = true;
+        }
+        Assert.assertTrue(authorizationFailed);
+    }
+    
+    @Test
+    public void testCommitFailure() throws RemoteException, NotBoundException, OWLOntologyCreationException, OWLServerException {
+        OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+        createFergersonsPizza();
+        Client guestClient = TestUtilities.createClient(rmiPort, GUEST);
+        RemoteOntologyDocument remotePizza = (RemoteOntologyDocument) guestClient.getServerDocument(fergersonsPizzaLoc);
+        boolean authorizationFailed = false;
+        try {
+            TestUtilities.rawCommit(guestClient, remotePizza, new OntologyDocumentRevision(1), new RemoveAxiom(ontology, CHEESEY_PIZZA_DEFINITION));
+        }
+        catch (AuthorizationFailedException afe) {
+            authorizationFailed = true;
+        }
+        Assert.assertTrue(authorizationFailed);
+    }
+    
+    private void createFergersonsPizza() throws RemoteException, NotBoundException, OWLOntologyCreationException, OWLServerException {
+        Client fergersonsClient = TestUtilities.createClient(rmiPort, FERGERSON);
+        OWLOntology pizza = PizzaVocabulary.loadPizza();
+        ClientUtilities.createServerOntology(fergersonsClient, fergersonsPizzaLoc, new ChangeMetaData(), pizza);
+    }
+    
+    private void createRedmondsPrivateDir() throws RemoteException, NotBoundException, OWLServerException, OWLOntologyCreationException {
+        Client redmondsClient = TestUtilities.createClient(rmiPort, REDMOND);
+        redmondsClient.createRemoteDirectory(redmondsPrivateDirLoc);
+        OWLOntology pizza = PizzaVocabulary.loadPizza();
+        ClientUtilities.createServerOntology(redmondsClient, redmondsPrivatePizzaLoc, new ChangeMetaData(), pizza);
     }
 }
