934323c8bca52e0b6381b2a019cc13d3851f3860
Timothy Redmond <tredmond@stanford.edu>
2012-10-17 23:46:28 +0000

    still trying to find the cause of gforge 4298

---
+++ src/test/java/org/protege/owl/server/experiments/ReadChangeDocument.java
@@ -0,0 +1,23 @@
+package org.protege.owl.server.experiments;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.protege.owl.server.api.DocumentFactory;
+import org.protege.owl.server.changes.DocumentFactoryImpl;
+
+public class ReadChangeDocument {
+
+    /**
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+        DocumentFactory factory = new DocumentFactoryImpl();
+        InputStream in = new FileInputStream(new File("src/test/resources/Pizza-ubuntu.history"));
+        factory.readChangeDocument(in, null, null);
+    }
+
+}
