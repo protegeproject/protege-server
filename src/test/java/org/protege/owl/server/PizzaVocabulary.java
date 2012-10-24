cc344b27c34b488b3f291ff60ca0946f3e70516a
Timothy Redmond <tredmond@stanford.edu>
2012-10-24 01:45:22 +0000

    added a few entries for debugging convenience

--- src/test/java/org/protege/owl/server/PizzaVocabulary.java
+++ src/test/java/org/protege/owl/server/PizzaVocabulary.java
@@ -21,11 +21,13 @@ public class PizzaVocabulary {
 	public static final OWLClass CHEESEY_PIZZA;
 	public static final OWLClass PIZZA;
     public static final OWLClass TOMATO_TOPPING;
+    public static final OWLClass VEGETABLE_TOPPING;
 	
 	public static final OWLObjectProperty HAS_TOPPING;
 	
 	public static final OWLAxiom CHEESEY_PIZZA_DEFINITION;
 	public static final OWLAxiom NOT_CHEESEY_PIZZA_DEFINITION;
+	public static final OWLAxiom VEGI_CHEESEY_PIZZA_DEFINITION;
 	public static final OWLAxiom HAS_TOPPING_DOMAIN;
 	
 	static {
@@ -34,11 +36,13 @@ public class PizzaVocabulary {
 		CHEESEY_PIZZA = factory.getOWLClass(IRI.create(PIZZA_NS + "#CheeseyPizza"));
 		PIZZA = factory.getOWLClass(IRI.create(PIZZA_NS + "#Pizza"));
 		TOMATO_TOPPING = factory.getOWLClass(IRI.create(PIZZA_NS + "#TomatoTopping"));
+		VEGETABLE_TOPPING = factory.getOWLClass(IRI.create(PIZZA_NS + "#VegetableTopping"));
 		
 		HAS_TOPPING = factory.getOWLObjectProperty(IRI.create(PIZZA_NS + "#hasTopping"));
 		
         CHEESEY_PIZZA_DEFINITION = factory.getOWLEquivalentClassesAxiom(CHEESEY_PIZZA, factory.getOWLObjectIntersectionOf(PIZZA, factory.getOWLObjectSomeValuesFrom(HAS_TOPPING, CHEESE_TOPPING)));
         NOT_CHEESEY_PIZZA_DEFINITION = factory.getOWLEquivalentClassesAxiom(CHEESEY_PIZZA, factory.getOWLObjectIntersectionOf(PIZZA, factory.getOWLObjectSomeValuesFrom(HAS_TOPPING, TOMATO_TOPPING)));
+        VEGI_CHEESEY_PIZZA_DEFINITION = factory.getOWLEquivalentClassesAxiom(CHEESEY_PIZZA, factory.getOWLObjectIntersectionOf(PIZZA, factory.getOWLObjectSomeValuesFrom(HAS_TOPPING, VEGETABLE_TOPPING)));        
 		HAS_TOPPING_DOMAIN = factory.getOWLObjectPropertyDomainAxiom(HAS_TOPPING, PIZZA);
 	}
 	
