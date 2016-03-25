d3a37fd2d3e5edfb01afa2e2e80363f128033ba5
johardi <josef.hardi@stanford.edu>
2016-03-25 15:02:33 -0700

    Code reformatting

--- src/main/java/org/protege/owl/server/api/DocumentFactory.java
+++ src/main/java/org/protege/owl/server/api/DocumentFactory.java
@@ -6,39 +6,33 @@ import java.util.List;
 
 import org.protege.owl.server.api.client.RemoteOntologyDocument;
 import org.protege.owl.server.api.client.VersionedOntologyDocument;
+import org.protege.owl.server.changes.OntologyDocumentRevision;
+import org.protege.owl.server.changes.api.SingletonChangeHistory;
+
 import org.semanticweb.owlapi.io.OWLObjectRenderer;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
 public interface DocumentFactory {
-    
+
     ChangeHistory createEmptyChangeDocument(OntologyDocumentRevision revision);
-	
-	/**
-	 * Create a change document for the list of changes and the commit comments.
-	 *
-	 * @param changes	changes
-	 * @param metaData	metaData
-	 * @param start	start
-	 * @return SingletonChangeHistory
-	 */
-	SingletonChangeHistory createChangeDocument(List<OWLOntologyChange> changes, ChangeMetaData metaData, OntologyDocumentRevision start);
-	
-	ChangeHistory readChangeDocument(InputStream in, OntologyDocumentRevision start, OntologyDocumentRevision end) throws IOException;
-	
-	boolean hasServerMetadata(OWLOntology ontology);
-	
-	IRI getServerLocation(OWLOntology ontology) throws IOException;
-	
-	boolean hasServerMetadata(IRI ontologyDocumentLocation);
-	
-	IRI getServerLocation(IRI ontologyDocumentLocation) throws IOException;
 
-	VersionedOntologyDocument getVersionedOntologyDocument(OWLOntology ontology) throws IOException;
-	
-	VersionedOntologyDocument createVersionedOntology(OWLOntology ontology, RemoteOntologyDocument serverDocument, OntologyDocumentRevision revision);
-	
-	OWLObjectRenderer getOWLRenderer();
+    SingletonChangeHistory createChangeDocument(List<OWLOntologyChange> changes, ChangeMetaData metadata, OntologyDocumentRevision start);
 
+    ChangeHistory readChangeDocument(InputStream in, OntologyDocumentRevision start, OntologyDocumentRevision end) throws IOException;
+
+    boolean hasServerMetadata(OWLOntology ontology);
+
+    IRI getServerLocation(OWLOntology ontology) throws IOException;
+
+    boolean hasServerMetadata(IRI ontologyDocumentLocation);
+
+    IRI getServerLocation(IRI ontologyDocumentLocation) throws IOException;
+
+    VersionedOntologyDocument getVersionedOntologyDocument(OWLOntology ontology) throws IOException;
+
+    VersionedOntologyDocument createVersionedOntology(OWLOntology ontology, RemoteOntologyDocument remoteDocument, OntologyDocumentRevision revision);
+
+    OWLObjectRenderer getOWLRenderer();
 }
