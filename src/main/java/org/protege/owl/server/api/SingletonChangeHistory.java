6fc0afbbcb0225166de0c14b6d2d769cd307fd7f
johardi <josef.hardi@stanford.edu>
2016-03-25 15:00:41 -0700

    Code reformatting

--- src/main/java/org/protege/owl/server/api/SingletonChangeHistory.java
+++ src/main/java/org/protege/owl/server/api/SingletonChangeHistory.java
@@ -1,13 +1,11 @@
 package org.protege.owl.server.api;
 
 /**
- * Represents a change history where the end revision is exactly one revision later than the 
- * original revision.
- * 
- * @author redmond
+ * Represents a change history where the end revision is exactly one revision
+ * later than the original revision.
  *
+ * @author redmond
  */
-@Deprecated
 public interface SingletonChangeHistory extends ChangeHistory {
-
+    // NO-OP
 }
