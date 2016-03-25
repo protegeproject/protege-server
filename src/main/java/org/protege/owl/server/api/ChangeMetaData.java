946e3a08ecec2867b7e9fec618ec47f4d11fb10f
johardi <josef.hardi@stanford.edu>
2016-03-25 15:05:58 -0700

    Code reformatting

--- src/main/java/org/protege/owl/server/api/ChangeMetaData.java
+++ src/main/java/org/protege/owl/server/api/ChangeMetaData.java
@@ -3,59 +3,68 @@ package org.protege.owl.server.api;
 import java.io.Serializable;
 import java.util.Date;
 
+import edu.stanford.protege.metaproject.api.AuthToken;
+import edu.stanford.protege.metaproject.api.UserId;
+
 public final class ChangeMetaData implements Serializable {
+
     private static final long serialVersionUID = -1198003999159038367L;
+
     private Date date;
-    private String commitComment;
-    private UserId userId = new UserId("");
-    
-    public ChangeMetaData(Date date, String commitComment) {
+    private String comment;
+    private UserId userId;
+
+    public ChangeMetaData(Date date, String comment) {
         this.date = date;
-        this.commitComment = commitComment;
+        this.comment = comment;
     }
-    public ChangeMetaData(String commitComment) {
+
+    public ChangeMetaData(String comment) {
         this.date = new Date();
-        this.commitComment = commitComment;
+        this.comment = comment;
     }
-    
+
     public ChangeMetaData() {
         this.date = new Date();
-        this.commitComment = "";
+        this.comment = "";
     }
-    
+
     public Date getDate() {
         return date;
     }
+
     public String getCommitComment() {
-        return commitComment;
+        return comment;
     }
-    
+
     public UserId getUserId() {
         return userId;
     }
-    
+
     public void setUser(AuthToken user) {
         this.userId = user.getUserId();
     }
 
     @Override
     public int hashCode() {
-        return date.hashCode() + 42 * commitComment.hashCode() + userId.hashCode()/42;
+        return date.hashCode() + 42 * comment.hashCode() + userId.hashCode() / 42;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
         if (!(obj instanceof ChangeMetaData)) {
             return false;
         }
         ChangeMetaData other = (ChangeMetaData) obj;
-        return other.getCommitComment().equals(commitComment) && other.getDate().equals(date) && userId.equals(other.getUserId());
+        return other.getCommitComment().equals(comment) && other.getDate().equals(date)
+                && userId.equals(other.getUserId());
     }
 
-    
     @Override
     public String toString() {
-        return "Committed at " + date +": " + commitComment;
+        return "Committed at " + date + ": " + comment;
     }
-
 }
