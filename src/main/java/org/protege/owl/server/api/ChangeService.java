e92a2435004cddf18a3a299f65214959c61b3156
johardi <josef.hardi@stanford.edu>
2016-03-29 10:08:34 -0700

    Add service for computing document changes.

--- src/main/java/org/protege/owl/server/api/ChangeService.java
+++ src/main/java/org/protege/owl/server/api/ChangeService.java
@@ -2,20 +2,27 @@ package org.protege.owl.server.api;
 
 import org.protege.owl.server.changes.OntologyDocumentRevision;
 import org.protege.owl.server.changes.api.ChangeHistory;
-import org.protege.owl.server.changes.api.ServerOntologyDocument;
 
-import org.semanticweb.owlapi.model.OWLOntologyChange;
-
-import java.util.List;
+import java.io.File;
 
 /**
  * @author Josef Hardi <johardi@stanford.edu> <br>
- * Stanford Center for Biomedical Informatics Research
+ *         Stanford Center for Biomedical Informatics Research
  */
 public interface ChangeService {
 
-    ChangeHistory getChanges(ServerOntologyDocument doc, OntologyDocumentRevision startRevision,
+    /**
+     * Compute the changes given the history file and the revision range.
+     *
+     * @param historyFile
+     *            The input history file.
+     * @param startRevision
+     *            The start revision.
+     * @param endRevision
+     *            The end revision.
+     * @return The change history from the start revision until the end revision
+     * @throws Exception
+     */
+    ChangeHistory getChanges(File historyFile, OntologyDocumentRevision startRevision,
             OntologyDocumentRevision endRevision) throws Exception;
-
-    List<OWLOntologyChange> getConflicts(ServerOntologyDocument doc, CommitBundle changes) throws Exception;
 }
