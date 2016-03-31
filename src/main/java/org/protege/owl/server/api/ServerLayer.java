11de4884442dd47875b3efab2a10ab5fd4e3aaa5
johardi <josef.hardi@stanford.edu>
2016-03-31 10:17:00 -0700

    Remove method to get the history file from the server

--- src/main/java/org/protege/owl/server/api/ServerLayer.java
+++ src/main/java/org/protege/owl/server/api/ServerLayer.java
@@ -1,15 +1,6 @@
 package org.protege.owl.server.api;
 
-import org.protege.owl.server.api.exception.ServerRequestException;
-import org.protege.owl.server.api.server.ServerPath;
-import org.protege.owl.server.changes.HistoryFile;
-
-import java.io.FileNotFoundException;
-
-import edu.stanford.protege.metaproject.api.Project;
-import edu.stanford.protege.metaproject.api.ProjectId;
 import edu.stanford.protege.metaproject.api.ServerConfiguration;
-import edu.stanford.protege.metaproject.api.exception.UnknownProjectIdException;
 
 public abstract class ServerLayer implements Server {
 
@@ -19,22 +10,4 @@ public abstract class ServerLayer implements Server {
      * @return Server configuration
      */
     protected abstract ServerConfiguration getConfiguration();
-
-    protected HistoryFile getHistoryFile(ProjectId projectId, ServerPath serverPath) throws ServerRequestException {
-        try {
-            Project project = getConfiguration().getMetaproject().getProjectRegistry().getProject(projectId);
-            String rootPath = project.getAddress().get();
-            HistoryFile historyFile = new HistoryFile(rootPath, serverPath.pathAsString());
-            if (!historyFile.exists()) {
-                throw new FileNotFoundException(); // TODO: Use factory to create the history file
-            }
-            return historyFile;
-        }
-        catch (UnknownProjectIdException e) {
-            throw new ServerRequestException(e);
-        }
-        catch (FileNotFoundException e) {
-            throw new ServerRequestException(e);
-        }
-    }
 }
