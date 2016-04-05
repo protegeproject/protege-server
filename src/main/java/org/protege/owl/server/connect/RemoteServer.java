4a8ff2cb9a33b5deb2191bae71c29307362e8748
johardi <josef.hardi@stanford.edu>
2016-04-05 10:09:03 -0700

    The RemoteServer should extend Server only

--- src/main/java/org/protege/owl/server/connect/RemoteServer.java
+++ src/main/java/org/protege/owl/server/connect/RemoteServer.java
@@ -1,14 +1,12 @@
 package org.protege.owl.server.connect;
 
+import org.protege.owl.server.api.Server;
 import org.protege.owl.server.api.exception.ServerRequestException;
-import org.protege.owl.server.api.server.ServerRequests;
-
-import java.rmi.Remote;
 
 import edu.stanford.protege.metaproject.api.ClientConfiguration;
 import edu.stanford.protege.metaproject.api.UserId;
 
-public interface RemoteServer extends ServerRequests, Remote {
+public interface RemoteServer extends Server {
 
     ClientConfiguration getClientConfiguration(UserId userId) throws ServerRequestException;
 }
