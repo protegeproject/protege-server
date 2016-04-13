c0a8f206f5e169812494052f880d2f133e04ba19
johardi <josef.hardi@stanford.edu>
2016-04-13 13:41:44 -0700

    Create ServerDocument to handle remote resources

---
+++ src/main/java/org/protege/owl/server/changes/ServerDocument.java
@@ -0,0 +1,39 @@
+package org.protege.owl.server.changes;
+
+import org.protege.owl.server.changes.api.ChangeHistory;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import edu.stanford.protege.metaproject.api.Host;
+
+public class ServerDocument implements Serializable {
+
+    private static final long serialVersionUID = 8705283686868339846L;
+
+    private Host host;
+    private HistoryFile historyFile;
+
+    private ChangeHistory changeHistory;
+
+    public ServerDocument(Host host, HistoryFile historyFile) {
+        this.host = host;
+        this.historyFile = historyFile;
+    }
+
+    public Host getHost() {
+        return host;
+    }
+
+    public HistoryFile getHistoryFile() {
+        return historyFile;
+    }
+
+    public Optional<ChangeHistory> getChangeHistory() {
+        return Optional.ofNullable(changeHistory);
+    }
+
+    public void setChangeHistory(ChangeHistory changeHistory) {
+        this.changeHistory = changeHistory;
+    }
+}
