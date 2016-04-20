60b73157313de1903c75a9e748b77ac1b11f9581
johardi <josef.hardi@stanford.edu>
2016-04-20 15:23:51 -0700

    Add new authentication test

---
+++ src/test/java/org/protege/owl/editor/server/AuthenticationTest.java
@@ -0,0 +1,94 @@
+package org.protege.owl.editor.server;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+import org.protege.editor.owl.server.security.DefaultLoginService;
+import org.protege.editor.owl.server.security.SessionManager;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import edu.stanford.protege.metaproject.api.AuthToken;
+import edu.stanford.protege.metaproject.api.AuthenticationRegistry;
+import edu.stanford.protege.metaproject.api.Salt;
+import edu.stanford.protege.metaproject.api.SaltedPasswordDigest;
+import edu.stanford.protege.metaproject.api.User;
+import edu.stanford.protege.metaproject.api.UserId;
+import edu.stanford.protege.metaproject.api.UserRegistry;
+import edu.stanford.protege.metaproject.api.exception.UserNotRegisteredException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthenticationTest {
+
+    @Mock private UserId validUserId;
+    @Mock private User validUser;
+    @Mock private SaltedPasswordDigest validSaltedPassword;
+
+    @Mock private UserId invalidUserId;
+    @Mock private User invalidUser;
+    @Mock private SaltedPasswordDigest invalidSaltedPassword;
+
+    @Mock private Salt userSalt;
+
+    @Mock private AuthenticationRegistry authRegistry;
+    @Mock private UserRegistry userRegistry;
+    @Mock private SessionManager sessionManager;
+
+    private DefaultLoginService loginService;
+
+    @Before
+    public void setUp() throws Exception {
+        loginService = new DefaultLoginService(authRegistry, userRegistry, sessionManager);
+        
+        when(authRegistry.hasValidCredentials(validUserId, validSaltedPassword)).thenReturn(true);
+        when(userRegistry.get(validUserId)).thenReturn(validUser);
+        
+        when(authRegistry.hasValidCredentials(invalidUserId, invalidSaltedPassword)).thenReturn(false);
+        when(authRegistry.hasValidCredentials(invalidUserId, validSaltedPassword)).thenReturn(false);
+        when(authRegistry.hasValidCredentials(validUserId, invalidSaltedPassword)).thenReturn(false);
+        
+        when(authRegistry.getSalt(validUserId)).thenReturn(userSalt);
+        when(authRegistry.getSalt(invalidUserId)).thenThrow(new UserNotRegisteredException());
+    }
+
+    @Test
+    public void loginTest() throws Exception {
+        AuthToken token = loginService.login(validUserId, validSaltedPassword);
+        assertNotNull(token);
+    }
+
+    @Test(expected=Exception.class)
+    public void invalidLoginTest() throws Exception {
+        AuthToken token = loginService.login(invalidUserId, invalidSaltedPassword);
+        assertNull(token);
+    }
+
+    @Test(expected=Exception.class)
+    public void invalidUsernameTest() throws Exception {
+        AuthToken token = loginService.login(invalidUserId, validSaltedPassword);
+        assertNull(token);
+    }
+
+    @Test(expected=Exception.class)
+    public void invalidPasswordTest() throws Exception {
+        AuthToken token = loginService.login(validUserId, invalidSaltedPassword);
+        assertNull(token);
+    }
+
+    @Test
+    public void getEncryptionKeyTest() throws Exception {
+        Salt salt = loginService.getEncryptionKey(validUserId);
+        assertNotNull(salt);
+    }
+
+    @Test(expected=UserNotRegisteredException.class)
+    public void notGetEncryptionKeyTest() throws Exception {
+        Salt salt = loginService.getEncryptionKey(invalidUserId);
+        assertNull(salt);
+    }
+}
