c6a83bde78ee0b60eb8a03754ec65ebab2de9c85
johardi <josef.hardi@stanford.edu>
2016-04-20 17:19:27 -0700

    Make an implementation of CommitBundle

---
+++ src/main/java/org/protege/editor/owl/server/policy/CommitBundleImpl.java
@@ -0,0 +1,29 @@
+package org.protege.editor.owl.server.policy;
+
+import org.protege.editor.owl.server.api.CommitBundle;
+import org.protege.editor.owl.server.versioning.DocumentRevision;
+
+import org.semanticweb.owlapi.model.OWLOntologyChange;
+
+import java.util.List;
+
+public class CommitBundleImpl implements CommitBundle {
+
+    private List<OWLOntologyChange> changes;
+    private DocumentRevision startRevision;
+
+    public CommitBundleImpl(final List<OWLOntologyChange> changes, DocumentRevision headRevision) {
+        this.changes = changes;
+        this.startRevision = headRevision;
+    }
+
+    @Override
+    public List<OWLOntologyChange> getChanges() {
+        return changes;
+    }
+
+    @Override
+    public DocumentRevision getStartRevision() {
+        return startRevision;
+    }
+}
