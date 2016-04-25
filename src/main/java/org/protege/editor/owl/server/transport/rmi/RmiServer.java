f31d8f64a7448efd9edd10c963bc20c2158d9156
johardi <josef.hardi@stanford.edu>
2016-04-25 10:51:26 -0700

    Decouple the interface for (local) Server and RemoteServer instances

--- src/main/java/org/protege/editor/owl/server/transport/rmi/RmiServer.java
+++ src/main/java/org/protege/editor/owl/server/transport/rmi/RmiServer.java
@@ -2,8 +2,11 @@ package org.protege.editor.owl.server.transport.rmi;
 
 import org.protege.editor.owl.server.api.CommitBundle;
 import org.protege.editor.owl.server.api.Server;
+import org.protege.editor.owl.server.api.exception.AuthorizationException;
+import org.protege.editor.owl.server.api.exception.ServerServiceException;
 import org.protege.editor.owl.server.versioning.ServerDocument;
 
+import java.net.URI;
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +24,7 @@ import edu.stanford.protege.metaproject.api.UserId;
 
 /**
  * @author Josef Hardi <johardi@stanford.edu> <br>
- * Stanford Center for Biomedical Informatics Research
+ *         Stanford Center for Biomedical Informatics Research
  */
 public class RmiServer implements RemoteServer {
 
@@ -34,344 +37,205 @@ public class RmiServer implements RemoteServer {
     }
 
     @Override
-    public void createUser(AuthToken token, User newUser) throws RemoteException {
-        try {
-            server.createUser(token, newUser);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void createUser(AuthToken token, User newUser)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.createUser(token, newUser);
     }
 
     @Override
-    public void deleteUser(AuthToken token, UserId userId) throws RemoteException {
-        try {
-            server.deleteUser(token, userId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void deleteUser(AuthToken token, UserId userId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.deleteUser(token, userId);
     }
 
     @Override
-    public void updateUser(AuthToken token, UserId userId, User user) throws RemoteException {
-        try {
-            server.updateUser(token, userId, user);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void updateUser(AuthToken token, UserId userId, User user)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.updateUser(token, userId, user);
     }
 
     @Override
-    public void createProject(AuthToken token, Project newProject) throws RemoteException {
-        try {
-            server.createProject(token, newProject);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void createProject(AuthToken token, Project newProject)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.createProject(token, newProject);
     }
 
     @Override
-    public void deleteProject(AuthToken token, ProjectId projectId) throws RemoteException {
-        try {
-            server.deleteProject(token, projectId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void deleteProject(AuthToken token, ProjectId projectId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.deleteProject(token, projectId);
     }
 
     @Override
-    public void updateProject(AuthToken token, ProjectId projectId, Project newProject) throws RemoteException {
-        try {
-            server.updateProject(token, projectId, newProject);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void updateProject(AuthToken token, ProjectId projectId, Project newProject)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.updateProject(token, projectId, newProject);
     }
 
     @Override
-    public ServerDocument openProject(AuthToken token, ProjectId projectId) throws RemoteException {
-        try {
-            return server.openProject(token, projectId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public ServerDocument openProject(AuthToken token, ProjectId projectId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.openProject(token, projectId);
     }
 
     @Override
-    public void createRole(AuthToken token, Role newRole) throws RemoteException {
-        try {
-            server.createRole(token, newRole);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void createRole(AuthToken token, Role newRole)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.createRole(token, newRole);
     }
 
     @Override
-    public void deleteRole(AuthToken token, RoleId roleId) throws RemoteException {
-        try {
-            server.deleteRole(token, roleId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void deleteRole(AuthToken token, RoleId roleId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.deleteRole(token, roleId);
     }
 
     @Override
-    public void updateRole(AuthToken token, RoleId roleId, Role newRole) throws RemoteException {
-        try {
-            server.updateRole(token, roleId, newRole);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void updateRole(AuthToken token, RoleId roleId, Role newRole)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.updateRole(token, roleId, newRole);
     }
 
     @Override
-    public void createOperation(AuthToken token, Operation operation) throws RemoteException {
-        try {
-            server.createOperation(token, operation);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void createOperation(AuthToken token, Operation operation)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.createOperation(token, operation);
     }
 
     @Override
-    public void deleteOperation(AuthToken token, OperationId operationId) throws RemoteException {
-        try {
-            server.deleteOperation(token, operationId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void deleteOperation(AuthToken token, OperationId operationId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.deleteOperation(token, operationId);
     }
 
     @Override
     public void updateOperation(AuthToken token, OperationId operationId, Operation newOperation)
-            throws RemoteException {
-        try {
-            server.updateOperation(token, operationId, newOperation);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.updateOperation(token, operationId, newOperation);
     }
 
     @Override
-    public void assignRole(AuthToken token, UserId userId, ProjectId projectId, RoleId roleId) throws RemoteException {
-        try {
-            server.assignRole(token, userId, projectId, roleId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void assignRole(AuthToken token, UserId userId, ProjectId projectId, RoleId roleId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.assignRole(token, userId, projectId, roleId);
     }
 
     @Override
-    public void retractRole(AuthToken token, UserId userId, ProjectId projectId, RoleId roleId) throws RemoteException {
-        try {
-            server.retractRole(token, userId, projectId, roleId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void retractRole(AuthToken token, UserId userId, ProjectId projectId, RoleId roleId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.retractRole(token, userId, projectId, roleId);
     }
 
     @Override
-    public Host getHost(AuthToken token) throws RemoteException {
-        try {
-            return server.getHost(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public Host getHost(AuthToken token) throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getHost(token);
     }
 
     @Override
-    public void setHostAddress(AuthToken token, String hostAddress) throws RemoteException {
-        try {
-            server.setHostAddress(token, hostAddress);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void setHostAddress(AuthToken token, URI hostAddress)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.setHostAddress(token, hostAddress);
     }
 
     @Override
-    public void setSecondaryPort(AuthToken token, int portNumber) throws RemoteException {
-        try {
-            server.setSecondaryPort(token, portNumber);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void setSecondaryPort(AuthToken token, int portNumber)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.setSecondaryPort(token, portNumber);
     }
 
     @Override
-    public String getRootDirectory(AuthToken token) throws RemoteException {
-        try {
-            return server.getRootDirectory(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public String getRootDirectory(AuthToken token)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getRootDirectory(token);
     }
 
     @Override
-    public void setRootDirectory(AuthToken token, String rootDirectory) throws RemoteException {
-        try {
-            server.setRootDirectory(token, rootDirectory);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void setRootDirectory(AuthToken token, String rootDirectory)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.setRootDirectory(token, rootDirectory);
     }
 
     @Override
-    public Map<String, String> getServerProperties(AuthToken token) throws RemoteException {
-        try {
-            return server.getServerProperties(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public Map<String, String> getServerProperties(AuthToken token)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getServerProperties(token);
     }
 
     @Override
-    public void setServerProperty(AuthToken token, String property, String value) throws RemoteException {
-        try {
-            server.setServerProperty(token, property, value);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void setServerProperty(AuthToken token, String property, String value)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.setServerProperty(token, property, value);
     }
 
     @Override
-    public void unsetServerProperty(AuthToken token, String property) throws RemoteException {
-        try {
-            server.unsetServerProperty(token, property);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void unsetServerProperty(AuthToken token, String property)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.unsetServerProperty(token, property);
     }
 
     @Override
-    public void commit(AuthToken token, Project project, CommitBundle changes) throws RemoteException {
-        try {
-            server.commit(token, project, changes);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public void commit(AuthToken token, Project project, CommitBundle changes)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        server.commit(token, project, changes);
     }
 
     @Override
-    public List<User> getAllUsers(AuthToken token) throws RemoteException {
-        try {
-            return server.getAllUsers(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<User> getAllUsers(AuthToken token)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getAllUsers(token);
     }
 
     @Override
-    public List<Project> getProjects(AuthToken token, UserId userId) throws RemoteException {
-        try {
-            return server.getProjects(token, userId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<Project> getProjects(AuthToken token, UserId userId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getProjects(token, userId);
     }
 
     @Override
-    public List<Project> getAllProjects(AuthToken token) throws RemoteException {
-        try {
-            return server.getAllProjects(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<Project> getAllProjects(AuthToken token)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getAllProjects(token);
     }
 
     @Override
-    public Map<ProjectId, List<Role>> getRoles(AuthToken token, UserId userId) throws RemoteException {
-        try {
-            return server.getRoles(token, userId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public Map<ProjectId, List<Role>> getRoles(AuthToken token, UserId userId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getRoles(token, userId);
     }
 
     @Override
-    public List<Role> getRoles(AuthToken token, UserId userId, ProjectId projectId) throws RemoteException {
-        try {
-            return server.getRoles(token, userId, projectId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<Role> getRoles(AuthToken token, UserId userId, ProjectId projectId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getRoles(token, userId, projectId);
     }
 
     @Override
-    public List<Role> getAllRoles(AuthToken token) throws RemoteException {
-        try {
-            return server.getAllRoles(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<Role> getAllRoles(AuthToken token)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getAllRoles(token);
     }
 
     @Override
-    public Map<ProjectId, List<Operation>> getOperations(AuthToken token, UserId userId) throws RemoteException {
-        try {
-            return server.getOperations(token, userId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public Map<ProjectId, List<Operation>> getOperations(AuthToken token, UserId userId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getOperations(token, userId);
     }
 
     @Override
-    public List<Operation> getOperations(AuthToken token, UserId userId, ProjectId projectId) throws RemoteException {
-        try {
-            return server.getOperations(token, userId, projectId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<Operation> getOperations(AuthToken token, UserId userId, ProjectId projectId)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getOperations(token, userId, projectId);
     }
 
     @Override
-    public List<Operation> getAllOperations(AuthToken token) throws RemoteException {
-        try {
-            return server.getAllOperations(token);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+    public List<Operation> getAllOperations(AuthToken token)
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.getAllOperations(token);
     }
 
     @Override
     public boolean isOperationAllowed(AuthToken token, OperationId operationId, ProjectId projectId, UserId userId)
-            throws RemoteException {
-        try {
-            return server.isOperationAllowed(token, operationId, projectId, userId);
-        }
-        catch (Exception e) {
-            throw new RemoteException(e.getMessage(), e);
-        }
+            throws AuthorizationException, ServerServiceException, RemoteException {
+        return server.isOperationAllowed(token, operationId, projectId, userId);
     }
 }
