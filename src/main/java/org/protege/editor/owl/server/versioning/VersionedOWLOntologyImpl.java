407ab7645a07334b6d2b646c43f3b8feb3cd3e8b
johardi <josef.hardi@stanford.edu>
2016-05-12 15:04:13 -0700

    Code tidy

--- src/main/java/org/protege/editor/owl/server/versioning/VersionedOWLOntologyImpl.java
+++ src/main/java/org/protege/editor/owl/server/versioning/VersionedOWLOntologyImpl.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
