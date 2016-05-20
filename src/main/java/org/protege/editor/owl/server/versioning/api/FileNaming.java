7da52b0a56034e1ff9a6c3c368c45b1c014c12d8
johardi <josef.hardi@stanford.edu>
2016-05-20 15:27:39 -0700

    Constant class for file namings.

---
+++ src/main/java/org/protege/editor/owl/server/versioning/api/FileNaming.java
@@ -0,0 +1,10 @@
+package org.protege.editor.owl.server.versioning.api;
+
+public final class FileNaming {
+
+    public static final String HISTORY_EXT = ".history";
+
+    public static final String VERSIONING_DIR = ".owlserver"; // the period for hidden directory in UNIX
+
+    public static final String CONFIG_FILE = "config";
+}
