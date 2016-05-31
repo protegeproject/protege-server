1103f63b626dff756b8051fd489d584305c40641
Bob Dionne <dionne@dionne-associates.com>
2016-05-31 19:52:44 -0400

    Whitespace

--- src/main/java/org/protege/editor/owl/server/http/HTTPServer.java
+++ src/main/java/org/protege/editor/owl/server/http/HTTPServer.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import org.protege.editor.owl.server.api.ServerLayer;
 import org.protege.editor.owl.server.base.ProtegeServer;
 import org.protege.editor.owl.server.http.exception.ServerException;
 import org.protege.editor.owl.server.http.handlers.AuthenticationHandler;
@@ -22,7 +21,6 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import edu.stanford.protege.metaproject.Manager;
 import edu.stanford.protege.metaproject.api.AuthToken;
 import edu.stanford.protege.metaproject.api.AuthenticationRegistry;
-import edu.stanford.protege.metaproject.api.Metaproject;
 import edu.stanford.protege.metaproject.api.ServerConfiguration;
 import edu.stanford.protege.metaproject.api.UserRegistry;
 import edu.stanford.protege.metaproject.api.exception.ObjectConversionException;
@@ -38,7 +36,7 @@ import io.undertow.server.handlers.GracefulShutdownHandler;
 public final class HTTPServer {
 
 	private static final Logger LOGGER = Logger.getLogger(HTTPServer.class.getName());
-	
+
 	public static final String SERVER_CONFIGURATION_PROPERTY = "org.protege.owl.server.configuration";
 
 	public static final String DEFAULT_HOST = "localhost";
@@ -51,116 +49,116 @@ public final class HTTPServer {
 	public static final String PROJECTS = ROOT_PATH + "/meta/projects";
 	public static final String ALL_CHANGES = ROOT_PATH + "/all_changes"; 
 	public static final String COMMIT = ROOT_PATH + "/commit";
-	
+
 	private ServerConfiguration config;
-	
+
 	private ProtegeServer pserver;
-	
+
 	private SessionManager session_manager = new SessionManager();
 	private Map<String, AuthToken> token_map = new HashMap<String, AuthToken>();
-	
+
 	private Undertow web_server;
 	private boolean isRunning = false;
-	
-	
+
+
 	private URI uri;
 	private io.undertow.server.RoutingHandler router;
-	
-    private static HTTPServer server;
-	
+
+	private static HTTPServer server;
+
 	public static HTTPServer server() {
 		return server;
 	}
-	
+
 	public void addSession(String key, AuthToken tok) {
 		session_manager.add(tok);
 		token_map.put(key, tok);
-		
+
 	}
-	
+
 	public boolean validateToken(String user, String tok) {
 		AuthToken ltok = token_map.get(tok);
 		if (ltok != null) {
 			return user.equalsIgnoreCase(ltok.getUser().getId().get());
-			
+
 		} else {
 			return false;
 		}
 	}
-	
+
 	public HTTPServer() {server = this;}
 
-	
+
 	public static void main(final String[] args) {
-		
+
 		HTTPServer s = new HTTPServer();	
 		try {
 			s.start();
 		} catch (OWLOntologyCreationException | ServerException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void start() throws ServerException, OWLOntologyCreationException {
-		
-		
+
+
 		String config_fname = System.getProperty(SERVER_CONFIGURATION_PROPERTY);
-		
+
 		try {
-            config = Manager.getConfigurationManager().loadServerConfiguration(new File(config_fname));
-            
-            pserver = new ProtegeServer(config);           
-            
-            uri = config.getHost().getUri();
-            router = Handlers.routing();
-            
-        }
-        catch (FileNotFoundException e) {
-            throw new OWLOntologyCreationException(e);
-        }
-        catch (ObjectConversionException e) {
-            throw new OWLOntologyCreationException(e);
-        }
-		
+			config = Manager.getConfigurationManager().loadServerConfiguration(new File(config_fname));
+
+			pserver = new ProtegeServer(config);           
+
+			uri = config.getHost().getUri();
+			router = Handlers.routing();
+
+		}
+		catch (FileNotFoundException e) {
+			throw new OWLOntologyCreationException(e);
+		}
+		catch (ObjectConversionException e) {
+			throw new OWLOntologyCreationException(e);
+		}
+
 		// create login handler
 		AuthenticationRegistry authRegistry = config.getAuthenticationRegistry();
-        UserRegistry userRegistry = config.getMetaproject().getUserRegistry();
-        DefaultLoginService loginService = new DefaultLoginService(authRegistry, userRegistry, session_manager);
-        
-        BlockingHandler ls = new BlockingHandler(new HTTPLoginService(loginService));
-        
-        router.add("POST", LOGIN, ls);
-        
-        // create change service handler
-        AuthenticationHandler cs = new AuthenticationHandler(
-        		new BlockingHandler(
-        				new HTTPChangeService(
-        						pserver)));
-        
-        router.add("POST", COMMIT,  cs);
-        router.add("GET", ROOT_PATH + "/changes",  cs);
-        router.add("GET", ROOT_PATH + "/latest_changes",  cs);
-        router.add("POST", ALL_CHANGES,  cs);
-        
-        // create code generator handler
-        AuthenticationHandler cg = new AuthenticationHandler(
-        		new BlockingHandler(
-        				new CodeGenHandler(pserver)));
-        
-        router.add("GET", ROOT_PATH + "/gen_code", cg);
-        router.add("GET", ROOT_PATH + "/gen_codes", cg);
-        
-        // create mataproject handler        
-        AuthenticationHandler mp = new AuthenticationHandler(
-        		new BlockingHandler(
-        				new MetaprojectHandler(pserver)));
-        router.add("GET", ROOT_PATH + "/meta", mp);
-        router.add("POST", CREATE_PROJECT,  mp);
-        router.add("GET", PROJECT,  mp);
-        router.add("GET", PROJECTS, mp);
-        router.add("PUT", ROOT_PATH + "/meta", mp);      
-        
+		UserRegistry userRegistry = config.getMetaproject().getUserRegistry();
+		DefaultLoginService loginService = new DefaultLoginService(authRegistry, userRegistry, session_manager);
+
+		BlockingHandler ls = new BlockingHandler(new HTTPLoginService(loginService));
+
+		router.add("POST", LOGIN, ls);
+
+		// create change service handler
+		AuthenticationHandler cs = new AuthenticationHandler(
+				new BlockingHandler(
+						new HTTPChangeService(
+								pserver)));
+
+		router.add("POST", COMMIT,  cs);
+		router.add("GET", ROOT_PATH + "/changes",  cs);
+		router.add("GET", ROOT_PATH + "/latest_changes",  cs);
+		router.add("POST", ALL_CHANGES,  cs);
+
+		// create code generator handler
+		AuthenticationHandler cg = new AuthenticationHandler(
+				new BlockingHandler(
+						new CodeGenHandler(pserver)));
+
+		router.add("GET", ROOT_PATH + "/gen_code", cg);
+		router.add("GET", ROOT_PATH + "/gen_codes", cg);
+
+		// create mataproject handler        
+		AuthenticationHandler mp = new AuthenticationHandler(
+				new BlockingHandler(
+						new MetaprojectHandler(pserver)));
+		router.add("GET", ROOT_PATH + "/meta", mp);
+		router.add("POST", CREATE_PROJECT,  mp);
+		router.add("GET", PROJECT,  mp);
+		router.add("GET", PROJECTS, mp);
+		router.add("PUT", ROOT_PATH + "/meta", mp);      
+
 
 		final ExceptionHandler aExceptionHandler = Handlers.exceptionHandler(router);
 
@@ -182,8 +180,8 @@ public final class HTTPServer {
 				exchange.endExchange();
 			}
 		});
-	
-	
+
+
 
 		web_server = Undertow.builder()
 				.addHttpListener(uri.getPort(), uri.getHost())
