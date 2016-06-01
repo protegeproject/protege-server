2a97b6ca5442792084b8d4bb327b368f3406a72c
johardi <josef.hardi@stanford.edu>
2016-06-01 16:26:45 -0700

    Add check permission for Metaproject-related operations

--- src/main/java/org/protege/editor/owl/server/policy/AccessControlFilter.java
+++ src/main/java/org/protege/editor/owl/server/policy/AccessControlFilter.java
@@ -1,14 +1,20 @@
 package org.protege.editor.owl.server.policy;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.protege.editor.owl.server.api.CommitBundle;
 import org.protege.editor.owl.server.api.PerOperationCommitBundle;
 import org.protege.editor.owl.server.api.ServerFilterAdapter;
 import org.protege.editor.owl.server.api.ServerLayer;
+import org.protege.editor.owl.server.api.exception.AuthorizationException;
 import org.protege.editor.owl.server.api.exception.OperationNotAllowedException;
 import org.protege.editor.owl.server.api.exception.ServerServiceException;
 import org.protege.editor.owl.server.versioning.Commit;
 import org.protege.editor.owl.server.versioning.api.ChangeHistory;
-
+import org.protege.editor.owl.server.versioning.api.ServerDocument;
 import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.AddImport;
 import org.semanticweb.owlapi.model.AddOntologyAnnotation;
@@ -18,13 +24,19 @@ import org.semanticweb.owlapi.model.RemoveImport;
 import org.semanticweb.owlapi.model.RemoveOntologyAnnotation;
 import org.semanticweb.owlapi.model.SetOntologyID;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.stanford.protege.metaproject.api.AuthToken;
+import edu.stanford.protege.metaproject.api.Description;
 import edu.stanford.protege.metaproject.api.MetaprojectAgent;
+import edu.stanford.protege.metaproject.api.Name;
 import edu.stanford.protege.metaproject.api.Operation;
+import edu.stanford.protege.metaproject.api.OperationId;
+import edu.stanford.protege.metaproject.api.Project;
 import edu.stanford.protege.metaproject.api.ProjectId;
+import edu.stanford.protege.metaproject.api.ProjectOptions;
+import edu.stanford.protege.metaproject.api.Role;
+import edu.stanford.protege.metaproject.api.RoleId;
+import edu.stanford.protege.metaproject.api.SaltedPasswordDigest;
+import edu.stanford.protege.metaproject.api.User;
 import edu.stanford.protege.metaproject.api.UserId;
 import edu.stanford.protege.metaproject.api.exception.MetaprojectException;
 import edu.stanford.protege.metaproject.impl.Operations;
@@ -42,6 +54,149 @@ public class AccessControlFilter extends ServerFilterAdapter {
     public AccessControlFilter(ServerLayer delegate) {
         super(delegate);
         metaprojectAgent = getConfiguration().getMetaproject().getMetaprojectAgent();
+    }
+
+    protected void checkPermission(User user, Operation operation) throws ServerServiceException {
+        if (!metaprojectAgent.isOperationAllowed(operation.getId(), user.getId())) {
+            OperationNotAllowedException e = new OperationNotAllowedException(operation);
+            throw new ServerServiceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void createUser(AuthToken token, User newUser, Optional<SaltedPasswordDigest> password)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.ADD_USER);
+        super.createUser(token, newUser, password);
+    }
+
+    @Override
+    public void deleteUser(AuthToken token, UserId userId) throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.REMOVE_USER);
+        super.deleteUser(token, userId);
+    }
+
+    @Override
+    public void updateUser(AuthToken token, UserId userId, User user)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_USER);
+        super.updateUser(token, userId, user);
+    }
+
+    @Override
+    public ServerDocument createProject(AuthToken token, ProjectId projectId, Name projectName, Description description,
+            UserId owner, Optional<ProjectOptions> options) throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.ADD_PROJECT);
+        return super.createProject(token, projectId, projectName, description, owner, options);
+    }
+
+    @Override
+    public void deleteProject(AuthToken token, ProjectId projectId, boolean includeFile)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.REMOVE_PROJECT);
+        super.deleteProject(token, projectId, includeFile);
+    }
+
+    @Override
+    public void updateProject(AuthToken token, ProjectId projectId, Project newProject)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_PROJECT);
+        super.updateProject(token, projectId, newProject);
+    }
+
+    @Override
+    public ServerDocument openProject(AuthToken token, ProjectId projectId)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.OPEN_PROJECT);
+        return super.openProject(token, projectId);
+    }
+
+    @Override
+    public void createRole(AuthToken token, Role newRole) throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.ADD_ROLE);
+        super.createRole(token, newRole);
+    }
+
+    @Override
+    public void deleteRole(AuthToken token, RoleId roleId) throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.REMOVE_ROLE);
+        super.deleteRole(token, roleId);
+    }
+
+    @Override
+    public void updateRole(AuthToken token, RoleId roleId, Role newRole)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_ROLE);
+        super.updateRole(token, roleId, newRole);
+    }
+
+    @Override
+    public void createOperation(AuthToken token, Operation operation)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.ADD_OPERATION);
+        super.createOperation(token, operation);
+    }
+
+    @Override
+    public void deleteOperation(AuthToken token, OperationId operationId)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.REMOVE_OPERATION);
+        super.deleteOperation(token, operationId);
+    }
+
+    @Override
+    public void updateOperation(AuthToken token, OperationId operationId, Operation newOperation)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_OPERATION);
+        super.updateOperation(token, operationId, newOperation);
+    }
+
+    @Override
+    public void assignRole(AuthToken token, UserId userId, ProjectId projectId, RoleId roleId)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.ASSIGN_ROLE);
+        super.assignRole(token, userId, projectId, roleId);
+    }
+
+    @Override
+    public void retractRole(AuthToken token, UserId userId, ProjectId projectId, RoleId roleId)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.RETRACT_ROLE);
+        super.retractRole(token, userId, projectId, roleId);
+    }
+
+    @Override
+    public void setHostAddress(AuthToken token, URI hostAddress) throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_SERVER_CONFIG);
+        super.setHostAddress(token, hostAddress);
+    }
+
+    @Override
+    public void setSecondaryPort(AuthToken token, int portNumber)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_SERVER_CONFIG);
+        super.setSecondaryPort(token, portNumber);
+    }
+
+    @Override
+    public void setRootDirectory(AuthToken token, String rootDirectory)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_SERVER_CONFIG);
+        super.setRootDirectory(token, rootDirectory);
+    }
+
+    @Override
+    public void setServerProperty(AuthToken token, String property, String value)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_SERVER_CONFIG);
+        super.setServerProperty(token, property, value);
+    }
+
+    @Override
+    public void unsetServerProperty(AuthToken token, String property)
+            throws AuthorizationException, ServerServiceException {
+        checkPermission(token.getUser(), Operations.MODIFY_SERVER_CONFIG);
+        super.unsetServerProperty(token, property);
     }
 
     @Override
