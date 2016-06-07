f4d1bbd3c4f438cce3d26c46f491dde91bd5efc7
johardi <josef.hardi@stanford.edu>
2016-06-07 15:37:19 -0700

    Fix wrong log label

--- src/main/java/org/protege/editor/owl/server/base/ProtegeServer.java
+++ src/main/java/org/protege/editor/owl/server/base/ProtegeServer.java
@@ -490,7 +490,7 @@ public class ProtegeServer extends ServerLayer {
             changeHistory.addRevision(commit.getMetadata(), commit.getChanges());
             String message = String.format("Receive revision %s: %s",
                     changeHistory.getHeadRevision(), commit.getMetadata().getComment());
-            logger.error(printLog(token.getUser(), "Commit changes", message));
+            logger.info(printLog(token.getUser(), "Commit changes", message));
         }
         return changeHistory;
     }
