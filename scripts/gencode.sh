360d6b0136252068e7cb465fa465629e0f4c2b02
Bob Dionne <dionne@dionne-associates.com>
2016-06-08 13:40:09 -0400

    Some useful scripts for interacting with and smoke testing server

---
+++ scripts/gencode.sh
@@ -0,0 +1,14 @@
+user=$1
+password=$2
+curl -v -X POST -H "Content-Type:application/json" http://localhost:8080/nci_protege/login -d "{\"user\":\"$user\", \"password\":\"$password\"}" | jq --raw-output '. | .userid, .token' > usertok
+
+res=""
+for i in `cat usertok`
+do
+    res="${res}${i}"
+    res="${res}:"
+done
+AUTH=`echo -n ${res%?} | openssl enc -base64 | tr -d "\n"`
+echo $AUTH
+
+curl -v -H "Authorization: Basic ${AUTH}" http://localhost:8080/nci_protege/gen_code
---
+++ src/main/resources/server-template/runhttp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+java -Xmx3500M -Xms250M \
+     -Dorg.protege.owl.server.configuration=server-configuration.json \
+     -cp "bundles/*:bin/*" \
+     org.protege.editor.owl.server.http.HTTPServer
