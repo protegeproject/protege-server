9f23cd5b74a89dff387cc35de302c43a6d3cc8c1
Bob Dionne <dionne@dionne-associates.com>
2016-07-06 16:48:36 -0400

    Increase maximum memory of server to 4G

--- src/main/resources/server-template/run.sh
+++ src/main/resources/server-template/run.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-java -Xmx3500M -Xms250M \
+java -Xmx4000M -Xms1500M \
      -Dorg.protege.owl.server.configuration=server-configuration.json \
      -cp "bundles/*" \
      org.protege.editor.owl.server.http.HTTPServer
