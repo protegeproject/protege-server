88170d014bcd152787cc596ebd966071345b4e3f
johardi <josef.hardi@stanford.edu>
2016-07-26 16:49:49 -0700

    Fix the exception handling (#28)

--- src/main/java/org/protege/editor/owl/server/http/handlers/HTTPLoginService.java
+++ src/main/java/org/protege/editor/owl/server/http/handlers/HTTPLoginService.java
@@ -1,5 +1,6 @@
 package org.protege.editor.owl.server.http.handlers;
 
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.UUID;
 
@@ -8,6 +9,8 @@ import org.protege.editor.owl.server.http.HTTPServer;
 import org.protege.editor.owl.server.http.messages.HttpAuthResponse;
 import org.protege.editor.owl.server.http.messages.LoginCreds;
 import org.protege.editor.owl.server.security.DefaultLoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 import com.google.inject.Inject;
@@ -20,62 +23,74 @@ import edu.stanford.protege.metaproject.api.Salt;
 import edu.stanford.protege.metaproject.api.SaltedPasswordDigest;
 import edu.stanford.protege.metaproject.api.Serializer;
 import edu.stanford.protege.metaproject.api.UserId;
+import edu.stanford.protege.metaproject.api.exception.ObjectConversionException;
 import edu.stanford.protege.metaproject.serialization.DefaultJsonSerializer;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
 import io.undertow.util.StatusCodes;
 
-
 public class HTTPLoginService extends BaseRoutingHandler {
+	
+	private static Logger logger = LoggerFactory.getLogger(MetaprojectHandler.class);
 	
 	private DefaultLoginService loginService;
 	
 	@Inject
 	public HTTPLoginService(DefaultLoginService s) {
-		super();
-		this.loginService = s;
+		loginService = s;
 	}
 
 	@Override
-	public void handleRequest(final HttpServerExchange theExchange) {
-
-		Serializer<Gson> serl = new DefaultJsonSerializer();		
-
+	public void handleRequest(final HttpServerExchange exchange) {
+		Serializer<Gson> serl = new DefaultJsonSerializer();
+		/*
+		 * Read user's credential
+		 */
 		LoginCreds creds = null;
-		
 		try {
-			creds = (LoginCreds) serl.parse(new InputStreamReader(theExchange.getInputStream()), LoginCreds.class);
-
-
-			MetaprojectFactory f = Manager.getFactory();
-
-			UserId userId = f.getUserId(creds.getUser());
-			PlainPassword plainPassword = f.getPlainPassword(creds.getPassword());		
-			Salt userSalt;
-
-			userSalt = (Salt) loginService.getSalt(userId);
-
-			SaltedPasswordDigest passwordDigest = f.getPasswordHasher().hash(plainPassword, userSalt);
-
-			AuthToken authToken;
-
-			authToken = loginService.login(userId, passwordDigest);
-
-			
-			String key = UUID.randomUUID().toString();
-
-			HTTPServer.server().addSession(key, authToken);
-
-			theExchange.getResponseSender().send(serl.write(new HttpAuthResponse(key, authToken.getUser()), HttpAuthResponse.class));
-		} catch (ServerServiceException e) {
-			theExchange.setStatusCode(StatusCodes.UNAUTHORIZED);
-			theExchange.getResponseSender().send(serl.write(e.getMessage(), String.class));
-			
-		} catch (Exception e) {
-			theExchange.setStatusCode(StatusCodes.INTERNAL_SERVER_ERROR);
-
+			creds = (LoginCreds) serl.parse(new InputStreamReader(exchange.getInputStream()), LoginCreds.class);
 		}
-		theExchange.endExchange();
+		catch (FileNotFoundException | ObjectConversionException e) {
+			logger.error(e.getMessage(), e);
+			exchange.setStatusCode(StatusCodes.INTERNAL_SERVER_ERROR);
+			exchange.getResponseHeaders().add(new HttpString("Error-Message"), "Failed to read the login credential");
+			exchange.endExchange(); // end the request
+		}
+		
+		/*
+		 * Fetch the user's salt
+		 */
+		MetaprojectFactory f = Manager.getFactory();
+		UserId userId = f.getUserId(creds.getUser());
+		Salt userSalt = null;
+		try {
+			userSalt = (Salt) loginService.getSalt(userId);
+		}
+		catch (ServerServiceException e) {
+			logger.error(e.getMessage(), e);
+			exchange.setStatusCode(StatusCodes.UNAUTHORIZED);
+			exchange.getResponseHeaders().add(new HttpString("Error-Message"), e.getMessage());
+			exchange.endExchange(); // end the request
+		}
+		
+		/*
+		 * Call the login service and return the auth response
+		 */
+		PlainPassword plainPassword = f.getPlainPassword(creds.getPassword());
+		SaltedPasswordDigest passwordDigest = f.getPasswordHasher().hash(plainPassword, userSalt);
+		try {
+			AuthToken authToken = loginService.login(userId, passwordDigest);
+			String key = UUID.randomUUID().toString();
+			HTTPServer.server().addSession(key, authToken);
+			exchange.getResponseSender().send(serl.write(new HttpAuthResponse(key, authToken.getUser()), HttpAuthResponse.class));
+			exchange.endExchange();
+		}
+		catch (ServerServiceException e) {
+			logger.error(e.getMessage(), e);
+			exchange.setStatusCode(StatusCodes.UNAUTHORIZED);
+			exchange.getResponseHeaders().add(new HttpString("Error-Message"), e.getMessage());
+			exchange.endExchange(); // end the request
+		}
 	}
-
 }
 
