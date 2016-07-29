86e82b0c5fef3e0cb5428fabbe238179f4a0935c
Bob Dionne <dionne@dionne-associates.com>
2016-07-29 16:41:34 -0400

    Add bat file for starting server on windows

---
+++ src/main/resources/server-template/run.bat
@@ -0,0 +1,5 @@
+rem #!/bin/sh
+
+rem cd `dirname $0`
+
+java -Xmx4000M -Xms1500M -Dorg.protege.owl.server.configuration=server-configuration.json -cp "bundles/*" org.protege.editor.owl.server.http.HTTPServer
