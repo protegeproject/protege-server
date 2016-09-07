27c34390c574b20c5e49bb7da41d018133b12a7a
Bob Dionne <bitdiddle@icloud.com>
2016-09-07 07:08:55 -0400

    WIP - support for LDAP

--- src/main/java/org/protege/editor/owl/server/security/LDAPLoginService.java
+++ src/main/java/org/protege/editor/owl/server/security/LDAPLoginService.java
@@ -5,6 +5,10 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import org.protege.editor.owl.server.api.LoginService;
 import org.protege.editor.owl.server.api.exception.ServerServiceException;
 
+import com.unboundid.ldap.sdk.LDAPBindException;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
+
 import edu.stanford.protege.metaproject.api.AuthToken;
 import edu.stanford.protege.metaproject.api.Password;
 import edu.stanford.protege.metaproject.api.ServerConfiguration;
@@ -15,23 +19,45 @@ import edu.stanford.protege.metaproject.impl.AuthorizedUserToken;
 public class LDAPLoginService implements LoginService {
 	
     private ServerConfiguration config;
+    
+    private static final String LDAPHOST = "ldap_host";
+    private static final String LDAPPORT = "ldap_port";
    
     public LDAPLoginService() {}   
 
     @Override
     public AuthToken login(UserId userid, Password password) throws ServerServiceException {
     	
-    	// TODO: implements logic based on specs from NCI, TBD.
+    	// TODO: implements logic based on specs from NCI, TBD
     	
-    	AuthToken authToken = null;
-		try {
-			authToken = new AuthorizedUserToken(config.getUser(userid));
-		} catch (UnknownUserIdException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        return authToken;
-    	
+    	try {
+    		String host = config.getProperty(LDAPHOST);
+    		int port = Integer.parseInt(config.getProperty(LDAPPORT));
+    		
+			LDAPConnection ldap = new LDAPConnection(host, port,
+					"CN=" + userid.get() + ",OU=NCI,OU=NIH,OU=AD,DC=Nih,DC=GOV", password.getPassword());
+			
+			
+			
+			if (ldap.isConnected()) {
+				try {
+					return new AuthorizedUserToken(config.getUser(userid));
+				} catch (UnknownUserIdException e) {
+					throw new ServerServiceException("Invalid user id", e);					
+				}
+				
+			} else {
+				throw new ServerServiceException("Bad LDAP connection");
+				
+			}
+		} catch (LDAPException e1) {
+			if (e1 instanceof LDAPBindException) {
+				LDAPBindException ex = (LDAPBindException) e1;
+				ex.getBindResult().getResultCode().intValue();
+				throw new ServerServiceException("Issue with LDAP " + ex.getBindResult().getResultString(), ex);
+			}
+			throw new ServerServiceException("Issue with LDAP ",e1);		
+		} 	
     }
 
 	@Override
